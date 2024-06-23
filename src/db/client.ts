657f2cdf69d1921b969e61e8a85451225639ad34
Abhi Thube <athube@abhithube.com>
2024-06-23 15:55:19 -0700

    Removed kysely from root package

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,17 +1,5 @@
-import { Kysely, PostgresDialect, type QueryCreator } from 'kysely'
-import type { DB } from 'kysely-codegen'
 import { Pool } from 'pg'
 
 export const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 })
-
-const dialect = new PostgresDialect({
-	pool,
-})
-
-export const db = new Kysely<DB>({
-	dialect,
-})
-
-export type Database = QueryCreator<DB>
