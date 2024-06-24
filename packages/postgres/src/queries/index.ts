15c5a31148907d474cd2a50d70c7e0d40f87cf91
Abhi Thube <athube@abhithube.com>
2024-06-24 08:16:03 -0700

    Added missing query exports

--- packages/postgres/src/queries/index.ts
+++ packages/postgres/src/queries/index.ts
@@ -1,3 +1,7 @@
 export * from './feeds'
 export * from './feed-entries'
 export * from './entries'
+export * from './profile-feed-entries'
+export * from './profile-feeds'
+export * from './profiles'
+export * from './users'
