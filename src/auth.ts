59d332741b518c37ca54a030d45f66b3b536da1f
Abhi Thube <athube@abhithube.com>
2024-06-24 05:46:53 -0700

    Created method for initializing auth client

--- src/auth.ts
+++ src/auth.ts
@@ -1,22 +1,24 @@
 import { NodePostgresAdapter } from '@lucia-auth/adapter-postgresql'
 import { Lucia } from 'lucia'
-import { pool } from './db/client'
+import type { Client, Pool } from 'pg'
 
-const adapter = new NodePostgresAdapter(pool, {
-	user: 'users',
-	session: 'sessions',
-})
+export function createAuth(client: Client | Pool) {
+	const adapter = new NodePostgresAdapter(client, {
+		user: 'users',
+		session: 'sessions',
+	})
 
-export const auth = new Lucia(adapter, {
-	sessionCookie: {
-		attributes: {
-			secure: process.env.NODE_ENV === 'production',
+	return new Lucia(adapter, {
+		sessionCookie: {
+			attributes: {
+				secure: process.env.NODE_ENV === 'production',
+			},
 		},
-	},
-})
+	})
+}
 
 declare module 'lucia' {
 	interface Register {
-		Lucia: typeof auth
+		Lucia: ReturnType<typeof createAuth>
 	}
 }
