5f592005e3839e582d4f06c7e4d649caced0bf63
Abhi Thube <athube@abhithube.com>
2024-06-25 10:43:50 -0700

    Implemented entries repository

---
+++ packages/postgres/src/repositories/entries.ts
@@ -0,0 +1,15 @@
+import type {
+	EntriesRepository,
+	Entry,
+	FindManyEntriesParams,
+} from '@colette/core'
+import type { Database } from '../client'
+import { selectProfileFeedEntries } from '../queries'
+
+export class EntrysPostgresRepository implements EntriesRepository {
+	constructor(private db: Database) {}
+
+	async findMany(data: FindManyEntriesParams): Promise<Entry[]> {
+		return selectProfileFeedEntries(this.db, data)
+	}
+}
--- packages/postgres/src/repositories/index.ts
+++ packages/postgres/src/repositories/index.ts
@@ -1,3 +1,4 @@
 export * from './profiles'
 export * from './users'
 export * from './feeds'
+export * from './entries'
