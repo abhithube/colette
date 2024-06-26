7c992ca6482511f5732b3940f69256e8396a3b93
Abhi Thube <athube@abhithube.com>
2024-06-26 13:51:50 -0700

    Added basic feeds service

---
+++ packages/core/src/feeds/service.ts
@@ -0,0 +1,35 @@
+import type { Session } from '../auth'
+import { FeedNotFoundError } from './error'
+import type { FeedsRepository } from './repository'
+
+export class FeedsService {
+	constructor(private repo: FeedsRepository) {}
+
+	async list(session: Session) {
+		return this.repo.findMany({
+			profileId: session.profileId,
+		})
+	}
+
+	async get(id: string, session: Session) {
+		const feed = this.repo.findOne({
+			id,
+			profileId: session.profileId,
+		})
+		if (!feed) {
+			throw new FeedNotFoundError(id)
+		}
+
+		return feed
+	}
+
+	async delete(id: string, session: Session) {
+		const deleted = this.repo.delete({
+			id,
+			profileId: session.profileId,
+		})
+		if (!deleted) {
+			throw new FeedNotFoundError(id)
+		}
+	}
+}
