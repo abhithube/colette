6ce875acebd8f5842f9765b6459b964f40e1b332
Abhi Thube <athube@abhithube.com>
2024-06-26 03:25:50 -0700

    Implemented profile CRUD methods

--- packages/postgres/src/repositories/profiles.ts
+++ packages/postgres/src/repositories/profiles.ts
@@ -1,20 +1,55 @@
 import type {
+	FindManyProfilesParams,
+	FindOneProfileOrDefaultParams,
+	FindOneProfileParams,
 	Profile,
 	ProfileCreateData,
 	ProfilesRepository,
 } from '@colette/core'
 import type { Database } from '../client'
-import { insertProfile } from '../queries'
+import {
+	deleteProfile,
+	insertProfile,
+	selectDefaultProfile,
+	selectProfileById,
+	selectProfiles,
+} from '../queries'
 
 export class ProfilesPostgresRepository implements ProfilesRepository {
 	constructor(private db: Database) {}
 
+	async findMany(params: FindManyProfilesParams): Promise<Profile[]> {
+		return selectProfiles(this.db, params)
+	}
+
+	async findOne(
+		params: FindOneProfileOrDefaultParams,
+	): Promise<Profile | null> {
+		const [profile] = params.id
+			? await selectProfileById(this.db, {
+					id: params.id,
+					userId: params.userId,
+				})
+			: await selectDefaultProfile(this.db, params)
+		if (!profile) {
+			return null
+		}
+
+		return profile
+	}
+
 	async create(data: ProfileCreateData): Promise<Profile> {
-		const rows = await insertProfile(this.db, data)
-		if (rows.length === 0) {
+		const [profile] = await insertProfile(this.db, data)
+		if (!profile) {
 			throw new Error('Profile not created')
 		}
 
-		return rows[0]
+		return profile
+	}
+
+	async delete(params: FindOneProfileParams): Promise<boolean> {
+		const result = await deleteProfile(this.db, params)
+
+		return result.rowCount === 1
 	}
 }
