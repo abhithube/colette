f09d4fc0c37322e8f3ddbec9ab2cff4da7dc60ea
Abhi Thube <athube@abhithube.com>
2024-06-28 07:13:53 -0700

    Added query to retrieve user by email

--- packages/core/src/users/repository.ts
+++ packages/core/src/users/repository.ts
@@ -1,5 +1,7 @@
-import type { User, UserCreateData } from './types'
+import type { FindOneUserParams, User, UserCreateData } from './types'
 
 export interface UsersRepository {
+	findOne(params: FindOneUserParams): Promise<User | null>
+
 	create(data: UserCreateData): Promise<User>
 }
--- packages/postgres/src/repositories/users.ts
+++ packages/postgres/src/repositories/users.ts
@@ -1,17 +1,27 @@
 import type {
+	FindOneUserParams,
 	User,
 	UserCreateData,
 	UsersRepository,
 	ValueGenerator,
 } from '@colette/core'
 import type { Database } from '../client'
-import { insertProfile, insertUser } from '../queries'
+import { insertProfile, insertUser, selectUserByEmail } from '../queries'
 
 export class UsersPostgresRepository implements UsersRepository {
 	constructor(
 		private readonly db: Database,
 		private readonly idGenerator: ValueGenerator<string>,
 	) {}
+
+	async findOne(params: FindOneUserParams): Promise<User | null> {
+		const [user] = await selectUserByEmail(this.db, params)
+		if (!user) {
+			return null
+		}
+
+		return user
+	}
 
 	async create(data: UserCreateData): Promise<User> {
 		return this.db.transaction(async (tx) => {
