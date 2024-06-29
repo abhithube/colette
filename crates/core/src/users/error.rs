a7846396c6c9d19eab3256a647d7b4e05084254e
Abhi Thube <athube@abhithube.com>
2024-06-29 11:16:04 -0700

    Added user not found error

--- crates/core/src/users/error.rs
+++ crates/core/src/users/error.rs
@@ -2,6 +2,9 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
+    #[error("user not found with email: {0}")]
+    NotFound(String),
+
     #[error("user already exists with email: {0}")]
     Conflict(String),
 
