96e185bb249ed2e90e3faa2128e5fcc577b1d7cd
Abhi Thube <athube@abhithube.com>
2024-06-29 09:34:12 -0700

    Added core crate

---
+++ crates/core/src/users/types.rs
@@ -0,0 +1,13 @@
+use serde::Serialize;
+use time::OffsetDateTime;
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    pub id: String,
+    pub email: String,
+    #[serde(skip_serializing)]
+    pub password: String,
+    pub created_at: OffsetDateTime,
+    pub updated_at: OffsetDateTime,
+}
