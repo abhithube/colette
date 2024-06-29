6bd018ba714a9bde1809e46990403d1581ec3165
Abhi Thube <athube@abhithube.com>
2024-06-29 06:10:04 -0700

    Fixed cookie header handling in middleware

--- packages/api/src/plugins/session.ts
+++ packages/api/src/plugins/session.ts
@@ -1,48 +1,42 @@
 import { type Session, UserNotAuthenticatedError } from '@colette/core'
-import Elysia, { t } from 'elysia'
+import Elysia from 'elysia'
 import { lucia } from '../deps'
 
-export default new Elysia()
-	.guard({
-		cookie: t.Cookie({
-			auth_session: t.Optional(t.String()),
-		}),
-	})
-	.derive({ as: 'scoped' }, async (ctx) => {
-		const cookie = ctx.cookie.auth_session.value
-		if (!cookie) {
-			throw new UserNotAuthenticatedError()
-		}
+export default new Elysia().derive({ as: 'scoped' }, async (ctx) => {
+	const cookieHeader = ctx.request.headers.get('Cookie')
+	if (!cookieHeader) {
+		throw new UserNotAuthenticatedError()
+	}
 
-		const sessionId = lucia.readSessionCookie(cookie)
-		if (!sessionId) {
-			throw new UserNotAuthenticatedError()
-		}
+	const sessionId = lucia.readSessionCookie(cookieHeader)
+	if (!sessionId) {
+		throw new UserNotAuthenticatedError()
+	}
 
-		const { session: luciaSession } = await lucia.validateSession(sessionId)
-		if (!luciaSession) {
-			const sessionCookie = lucia.createBlankSessionCookie()
-			ctx.cookie[sessionCookie.name].set({
-				value: sessionCookie.value,
-				...sessionCookie.attributes,
-			})
+	const { session: luciaSession } = await lucia.validateSession(sessionId)
+	if (!luciaSession) {
+		const sessionCookie = lucia.createBlankSessionCookie()
+		ctx.cookie[sessionCookie.name].set({
+			value: sessionCookie.value,
+			...sessionCookie.attributes,
+		})
 
-			throw new UserNotAuthenticatedError()
-		}
-		if (luciaSession.fresh) {
-			const sessionCookie = lucia.createSessionCookie(luciaSession.id)
-			ctx.cookie[sessionCookie.name].set({
-				value: sessionCookie.value,
-				...sessionCookie.attributes,
-			})
-		}
+		throw new UserNotAuthenticatedError()
+	}
+	if (luciaSession.fresh) {
+		const sessionCookie = lucia.createSessionCookie(luciaSession.id)
+		ctx.cookie[sessionCookie.name].set({
+			value: sessionCookie.value,
+			...sessionCookie.attributes,
+		})
+	}
 
-		const session: Session = {
-			userId: luciaSession.userId,
-			profileId: luciaSession.profileId,
-		}
+	const session: Session = {
+		userId: luciaSession.userId,
+		profileId: luciaSession.profileId,
+	}
 
-		return {
-			session,
-		}
-	})
+	return {
+		session,
+	}
+})
