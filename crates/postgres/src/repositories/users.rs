e03a595666f52ad9a07e72735158fc16ceb07c68
Abhi Thube <athube@abhithube.com>
2024-06-29 16:14:46 -0700

    Removed database pool reference

--- crates/postgres/src/repositories/users.rs
+++ crates/postgres/src/repositories/users.rs
@@ -7,16 +7,22 @@ use sqlx::PgPool;
 
 use crate::queries::users;
 
-pub struct UsersPostgresRepository<'a> {
-    pool: &'a PgPool,
+pub struct UsersPostgresRepository {
+    pool: PgPool,
+}
+
+impl UsersPostgresRepository {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
 }
 
 #[async_trait]
-impl UsersRepository for UsersPostgresRepository<'_> {
+impl UsersRepository for UsersPostgresRepository {
     async fn find_one(&self, params: UserFindOneParams) -> Result<User, Error> {
         let email = params.email.clone();
 
-        users::select_by_email(self.pool, params.into())
+        users::select_by_email(&self.pool, params.into())
             .await
             .map_err(|e| match e {
                 sqlx::Error::RowNotFound => Error::NotFound(email),
@@ -29,7 +35,7 @@ impl UsersRepository for UsersPostgresRepository<'_> {
     async fn create(&self, data: UserCreateData) -> Result<User, Error> {
         let email = data.email.clone();
 
-        let user = users::insert(self.pool, data.into())
+        let user = users::insert(&self.pool, data.into())
             .await
             .map_err(|e| match e {
                 sqlx::Error::Database(e) if e.is_unique_violation() => Error::Conflict(email),
