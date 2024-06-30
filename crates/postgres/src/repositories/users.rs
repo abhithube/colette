c6155f33054b78824b4b6b6950e34206a8270285
Abhi Thube <athube@abhithube.com>
2024-06-29 19:05:43 -0700

    Added default profile insert after user insert

--- crates/postgres/src/repositories/users.rs
+++ crates/postgres/src/repositories/users.rs
@@ -3,9 +3,10 @@ use colette_core::{
     users::{Error, UserCreateData, UserFindOneParams, UsersRepository},
     User,
 };
+use nanoid::nanoid;
 use sqlx::PgPool;
 
-use crate::queries::users;
+use crate::queries::{profiles, users};
 
 pub struct UsersPostgresRepository {
     pool: PgPool,
@@ -35,12 +36,31 @@ impl UsersRepository for UsersPostgresRepository {
     async fn create(&self, data: UserCreateData) -> Result<User, Error> {
         let email = data.email.clone();
 
-        let user = users::insert(&self.pool, data.into())
+        let mut tx = self
+            .pool
+            .begin()
+            .await
+            .map_err(|e| Error::Unknown(e.into()))?;
+
+        let user = users::insert(&mut *tx, data.into())
             .await
             .map_err(|e| match e {
                 sqlx::Error::Database(e) if e.is_unique_violation() => Error::Conflict(email),
                 _ => Error::Unknown(e.into()),
             })?;
+
+        let data = profiles::InsertData {
+            id: nanoid!(),
+            title: String::from("Default"),
+            image_url: None,
+            is_default: true,
+            user_id: user.id.clone(),
+        };
+        profiles::insert(&mut *tx, data)
+            .await
+            .map_err(|e| Error::Unknown(e.into()))?;
+
+        tx.commit().await.map_err(|e| Error::Unknown(e.into()))?;
 
         Ok(user)
     }
