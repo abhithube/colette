84b8d4f84614ef3ad57a3c91e77db693efe9c551
Abhi Thube <athube@abhithube.com>
2024-07-01 09:45:39 -0700

    Defined session key constant

--- crates/api/src/api.rs
+++ crates/api/src/api.rs
@@ -3,6 +3,8 @@ use std::sync::Arc;
 use axum::extract::FromRef;
 use colette_core::auth::AuthService;
 
+pub const SESSION_KEY: &str = "session";
+
 #[derive(Clone, FromRef)]
 pub struct Context {
     pub auth_service: Arc<AuthService>,
--- crates/api/src/session.rs
+++ crates/api/src/session.rs
@@ -1,8 +1,7 @@
 use axum::{async_trait, extract::FromRequestParts, http::request::Parts};
 use colette_core::{auth, common::Session};
-use tower_sessions::Session as TSession;
 
-use crate::error::Error;
+use crate::{api::SESSION_KEY, error::Error};
 
 pub struct SessionAuth(pub Session);
 
@@ -14,12 +13,12 @@ where
     type Rejection = Error;
 
     async fn from_request_parts(req: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let session = TSession::from_request_parts(req, state)
+        let session = tower_sessions::Session::from_request_parts(req, state)
             .await
             .map_err(|_| Error::Auth(auth::Error::NotAuthenticated))?;
 
         let user = session
-            .get::<Session>("session")
+            .get::<Session>(SESSION_KEY)
             .await
             .map_err(|_| Error::Auth(auth::Error::NotAuthenticated))?
             .ok_or(Error::Auth(auth::Error::NotAuthenticated))?;
