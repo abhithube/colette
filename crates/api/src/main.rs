80f1df3677ee221ad05654de37a91881663eb1b3
Abhi Thube <athube@abhithube.com>
2024-07-01 09:17:43 -0700

    Added session management middleware

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -4,17 +4,36 @@ use axum::Router;
 use colette_core::auth::AuthService;
 use colette_password::Argon2Hasher;
 use colette_postgres::repositories::users::UsersPostgresRepository;
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, task};
+use tower_sessions::{
+    cookie::time::Duration, session_store::ExpiredDeletion, Expiry, SessionManagerLayer,
+};
+use tower_sessions_sqlx_store::PostgresStore;
 
 mod api;
 mod auth;
 mod error;
+mod profiles;
+mod session;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let database_url = env::var("DATABASE_URL")?;
 
     let pool = colette_postgres::create_database(&database_url).await?;
+
+    let session_store = PostgresStore::new(pool.clone());
+    session_store.migrate().await?;
+
+    let deletion_task = task::spawn(
+        session_store
+            .clone()
+            .continuously_delete_expired(tokio::time::Duration::from_secs(60)),
+    );
+
+    let session_layer = SessionManagerLayer::new(session_store)
+        .with_secure(false)
+        .with_expiry(Expiry::OnInactivity(Duration::days(1)));
 
     let users_repository = Arc::new(UsersPostgresRepository::new(pool.clone()));
 
@@ -23,13 +42,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let state = api::Context { auth_service };
 
-    let app = Router::new().nest(
-        "/api",
-        Router::new().merge(auth::router()).with_state(state),
-    );
+    let app = Router::new()
+        .nest(
+            "/api",
+            Router::new().merge(auth::router()).with_state(state),
+        )
+        .layer(session_layer);
 
     let listener = TcpListener::bind("localhost:3001").await?;
     axum::serve(listener, app).await?;
+
+    deletion_task.await??;
 
     Ok(())
 }
