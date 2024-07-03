daaec306e86e4cad30310ccdc75f840dee508b16
Abhi Thube <athube@abhithube.com>
2024-07-03 11:10:46 -0700

    Added feed insert struct mapper

--- crates/database/src/feeds.rs
+++ crates/database/src/feeds.rs
@@ -1,6 +1,21 @@
+use colette_core::feeds::FeedCreateData;
+
 #[derive(Debug)]
 pub struct InsertData<'a> {
     pub link: &'a str,
     pub title: &'a str,
     pub url: Option<&'a str>,
+}
+
+impl<'a> From<&'a FeedCreateData> for InsertData<'a> {
+    fn from(value: &'a FeedCreateData) -> Self {
+        let link = value.feed.link.as_str();
+        let url = value.url.as_str();
+
+        Self {
+            link,
+            title: value.feed.title.as_str(),
+            url: if url == link { None } else { Some(url) },
+        }
+    }
 }
