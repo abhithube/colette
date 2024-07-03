c587a63940949e08764081ea2d87b22390bf7f72
Abhi Thube <athube@abhithube.com>
2024-07-02 23:27:34 -0700

    Added function to connect to SQLite database

--- crates/sqlite/src/lib.rs
+++ crates/sqlite/src/lib.rs
@@ -2,3 +2,8 @@ mod queries;
 mod repositories;
 
 pub use repositories::{profiles::ProfilesSqliteRepository, users::UsersSqliteRepository};
+use sqlx::{Error, SqlitePool};
+
+pub async fn create_database(url: &str) -> Result<SqlitePool, Error> {
+    SqlitePool::connect(url).await
+}
