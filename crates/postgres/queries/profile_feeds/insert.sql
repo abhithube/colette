8d02f2352069daee55acd5132737a799178eb2c6
Abhi Thube <athube@abhithube.com>
2024-07-04 11:44:51 -0700

    Fixed row not returned issue when upserting profile feed

--- crates/postgres/queries/profile_feeds/insert.sql
+++ crates/postgres/queries/profile_feeds/insert.sql
@@ -1,7 +1,22 @@
-INSERT INTO
-  profile_feeds (id, profile_id, feed_id)
-VALUES
-  ($1, $2, $3)
-ON CONFLICT (profile_id, feed_id) DO nothing
-RETURNING
-  id;
+WITH
+  pf AS (
+    INSERT INTO
+      profile_feeds (id, profile_id, feed_id)
+    VALUES
+      ($1, $2, $3)
+    ON CONFLICT (profile_id, feed_id) DO nothing
+    RETURNING
+      id
+  )
+SELECT
+  id "id!"
+FROM
+  pf
+UNION ALL
+SELECT
+  id
+FROM
+  profile_feeds
+WHERE
+  profile_id = $2
+  AND feed_id = $3;
