7d466068e48aa5d697a5f073bbcfbfc243adb1f5
Abhi Thube <athube@abhithube.com>
2024-07-06 22:15:26 -0700

    Defined entries repository

--- crates/core/src/entries/mod.rs
+++ crates/core/src/entries/mod.rs
@@ -1,3 +1,7 @@
+pub use error::Error;
 pub use model::{Entry, ListEntriesParams};
+pub use repository::{EntriesRepository, EntryFindManyParams};
 
+mod error;
 mod model;
+mod repository;
--- crates/database/src/profile_feed_entries.rs
+++ crates/database/src/profile_feed_entries.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, Utc};
+use colette_core::entries::EntryFindManyParams;
 
 #[derive(Debug)]
 pub struct SelectManyParams<'a> {
@@ -7,6 +8,18 @@ pub struct SelectManyParams<'a> {
     pub published_at: Option<&'a DateTime<Utc>>,
     pub profile_feed_id: Option<&'a str>,
     pub has_read: Option<bool>,
+}
+
+impl<'a> From<&'a EntryFindManyParams> for SelectManyParams<'a> {
+    fn from(value: &'a EntryFindManyParams) -> Self {
+        Self {
+            profile_id: &value.profile_id,
+            limit: value.limit,
+            published_at: value.published_at.as_ref(),
+            profile_feed_id: value.feed_id.as_deref(),
+            has_read: value.has_read,
+        }
+    }
 }
 
 #[derive(Debug)]
