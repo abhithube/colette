1658bfcf62b12dad7536c1b3044029b70706e388
Abhi Thube <athube@abhithube.com>
2024-07-07 19:53:32 -0700

    Tweaked base URL to be empty in prod

--- packages/solid-web/src/lib/client.ts
+++ packages/solid-web/src/lib/client.ts
@@ -2,7 +2,7 @@ import createClient from 'openapi-fetch'
 import type { paths } from './openapi'
 
 export const client = createClient<paths>({
-	baseUrl: import.meta.env.VITE_BACKEND_URL,
+	baseUrl: import.meta.env.DEV ? import.meta.env.VITE_BACKEND_URL : '',
 	credentials: 'include',
 })
 
