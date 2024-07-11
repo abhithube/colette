4a6762ca018c978839b0cbd76b02809889aa0dc6
Abhi Thube <athube@abhithube.com>
2024-07-11 02:36:38 -0700

    Set up OpenAPI client

---
+++ packages/react-web/src/lib/client.ts
@@ -0,0 +1,23 @@
+import createClient from 'openapi-fetch'
+import type { paths } from './openapi'
+
+export const client = createClient<paths>({
+	baseUrl: import.meta.env.DEV ? import.meta.env.VITE_BACKEND_URL : '',
+	credentials: 'include',
+})
+
+client.use({
+	onResponse: async ({ response }) => {
+		if (response.status >= 400) {
+			const body = await response.json()
+
+			if (response.status === 401) {
+				throw new AuthError(body)
+			}
+
+			throw new Error(body)
+		}
+	},
+})
+
+export class AuthError extends Error {}
