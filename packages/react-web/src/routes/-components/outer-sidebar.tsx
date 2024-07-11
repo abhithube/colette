5796e0797232c74659046edf3b4019b2776c746d
Abhi Thube <athube@abhithube.com>
2024-07-11 03:57:52 -0700

    Added icon tooltips to outer sidebar

--- packages/react-web/src/routes/-components/outer-sidebar.tsx
+++ packages/react-web/src/routes/-components/outer-sidebar.tsx
@@ -1,3 +1,9 @@
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/components/ui/tooltip'
 import { Bookmark, Home, Rss, Settings, User } from 'lucide-react'
 import { SidebarButton } from './sidebar-button'
 import { SidebarLink } from './sidebar-link'
@@ -5,22 +11,57 @@ import { SidebarLink } from './sidebar-link'
 export const OuterSidebar = () => {
 	return (
 		<nav className="flex h-full flex-col space-y-4 border-r p-4">
-			<SidebarLink to="/">
-				<Home className="h-5 w-5 shrink-0" />
-			</SidebarLink>
-			<SidebarLink to="/feeds">
-				<Rss className="h-5 w-5 shrink-0" />
-			</SidebarLink>
-			<SidebarLink to="/collections">
-				<Bookmark className="h-5 w-5 shrink-0" />
-			</SidebarLink>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger>
+						<SidebarLink to="/">
+							<Home className="h-5 w-5 shrink-0" />
+						</SidebarLink>
+					</TooltipTrigger>
+					<TooltipContent side="right">Home</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger>
+						<SidebarLink to="/feeds" activeOptions={{ exact: false }}>
+							<Rss className="h-5 w-5 shrink-0" />
+						</SidebarLink>
+					</TooltipTrigger>
+					<TooltipContent side="right">Feed</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger>
+						<SidebarLink to="/collections" activeOptions={{ exact: false }}>
+							<Bookmark className="h-5 w-5 shrink-0" />
+						</SidebarLink>
+					</TooltipTrigger>
+					<TooltipContent side="right">Bookmarks</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
 			<div className="flex-grow" />
-			<SidebarButton>
-				<User className="h-5 w-5 shrink-0" />
-			</SidebarButton>
-			<SidebarButton>
-				<Settings className="h-5 w-5 shrink-0" />
-			</SidebarButton>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<SidebarButton>
+							<User className="h-5 w-5 shrink-0" />
+						</SidebarButton>
+					</TooltipTrigger>
+					<TooltipContent side="right">Profile</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<SidebarButton>
+							<Settings className="h-5 w-5 shrink-0" />
+						</SidebarButton>
+					</TooltipTrigger>
+					<TooltipContent side="right">Settings</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
 		</nav>
 	)
 }
