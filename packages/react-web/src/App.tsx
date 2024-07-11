41269a88ef9cc6bb16aa23f3b8ef8e433b8186b3
Abhi Thube <athube@abhithube.com>
2024-07-11 01:34:45 -0700

    Added login form

--- packages/react-web/src/App.tsx
+++ packages/react-web/src/App.tsx
@@ -1,3 +1,71 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button } from './components/ui/button'
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from './components/ui/form'
+import { Input } from './components/ui/input'
+
+const formSchema = z.object({
+	email: z.string().email('Email is not valid.'),
+	password: z.string().min(8, 'Password must be 8 or more characters.'),
+})
+
+type Values = z.infer<typeof formSchema>
+
 export const App = () => {
-	return <div>Hello world!</div>
+	const form = useForm<Values>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	})
+
+	function onSubmit(values: Values) {
+		console.log(values)
+	}
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input placeholder="user@email.com" {...field} />
+							</FormControl>
+							<FormDescription>This is your email.</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Password</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormDescription>This is your password.</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit">Submit</Button>
+			</form>
+		</Form>
+	)
 }
