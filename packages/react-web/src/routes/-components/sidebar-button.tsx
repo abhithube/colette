023b458f5b7c3b90f0fdf464efea66cc487c7d88
Abhi Thube <athube@abhithube.com>
2024-07-11 10:36:17 -0700

    Added Tailwind class sorting lint rule

--- packages/react-web/src/routes/-components/sidebar-button.tsx
+++ packages/react-web/src/routes/-components/sidebar-button.tsx
@@ -5,7 +5,7 @@ export const SidebarButton = forwardRef<HTMLButtonElement, ButtonProps>(
 	({ children, ...props }, ref) => {
 		return (
 			<Button
-				className="flex w-full hover:bg-muted/50 items-center justify-between space-x-4 rounded-md px-4 py-2 font-medium text-primary text-sm"
+				className="flex w-full items-center justify-between space-x-4 rounded-md px-4 py-2 font-medium text-primary text-sm hover:bg-muted/50"
 				ref={ref}
 				variant="ghost"
 				{...props}
