609d27796df7f2119624ae907884f875deca3b3d
Abhi Thube <athube@abhithube.com>
2024-07-12 09:54:41 -0700

    Added dark mode support

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { ThemeProvider } from './components/theme-provider'
 import { routeTree } from './routeTree.gen'
 
 const queryClient = new QueryClient()
@@ -22,8 +23,10 @@ declare module '@tanstack/react-router' {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-		</QueryClientProvider>
+		<ThemeProvider>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
+		</ThemeProvider>
 	</React.StrictMode>,
 )
