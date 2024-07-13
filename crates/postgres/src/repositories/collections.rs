304cd7eb4d88a3f7a473e48e24c57425897999cb
Abhi Thube <athube@abhithube.com>
2024-07-13 01:09:14 -0700

    Implemented PostgreSQL collections repository

---
+++ crates/postgres/src/repositories/collections.rs
@@ -0,0 +1,60 @@
+use async_trait::async_trait;
+use colette_core::{
+    collections::{CollectionCreateData, CollectionFindManyParams, CollectionsRepository, Error},
+    common, Collection,
+};
+use sqlx::PgPool;
+
+use crate::queries;
+
+#[derive(Clone)]
+pub struct CollectionsPostgresRepository {
+    pool: PgPool,
+}
+
+impl CollectionsPostgresRepository {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+}
+
+#[async_trait]
+impl CollectionsRepository for CollectionsPostgresRepository {
+    async fn find_many(&self, params: CollectionFindManyParams) -> Result<Vec<Collection>, Error> {
+        let feeds = queries::collections::select_many(&self.pool, (&params).into())
+            .await
+            .map_err(|e| Error::Unknown(e.into()))?;
+
+        Ok(feeds)
+    }
+
+    async fn find_one(&self, params: common::FindOneParams) -> Result<Collection, Error> {
+        let feed = queries::collections::select_by_id(&self.pool, (&params).into())
+            .await
+            .map_err(|e| match e {
+                sqlx::Error::RowNotFound => Error::NotFound(params.id),
+                _ => Error::Unknown(e.into()),
+            })?;
+
+        Ok(feed)
+    }
+
+    async fn create(&self, data: CollectionCreateData) -> Result<Collection, Error> {
+        let collection = queries::collections::insert(&self.pool, (&data).into())
+            .await
+            .map_err(|e| Error::Unknown(e.into()))?;
+
+        Ok(collection)
+    }
+
+    async fn delete(&self, params: common::FindOneParams) -> Result<(), Error> {
+        queries::collections::delete(&self.pool, (&params).into())
+            .await
+            .map_err(|e| match e {
+                sqlx::Error::RowNotFound => Error::NotFound(params.id),
+                _ => Error::Unknown(e.into()),
+            })?;
+
+        Ok(())
+    }
+}
--- crates/postgres/src/repositories/mod.rs
+++ crates/postgres/src/repositories/mod.rs
@@ -1,8 +1,10 @@
+pub use collections::CollectionsPostgresRepository;
 pub use entries::EntriesPostgresRepository;
 pub use feeds::FeedsPostgresRepository;
 pub use profiles::ProfilesPostgresRepository;
 pub use users::UsersPostgresRepository;
 
+mod collections;
 mod entries;
 mod feeds;
 mod profiles;
