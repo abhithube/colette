c3b7f97a40af27e63e5ed1c9bedd72deabdb2957
Abhi Thube <athube@abhithube.com>
2024-07-13 01:10:53 -0700

    Implemented SQLite collections repository

--- crates/sqlite/src/repositories/mod.rs
+++ crates/sqlite/src/repositories/mod.rs
@@ -1,8 +1,10 @@
+pub use collections::CollectionsSqliteRepository;
 pub use entries::EntriesSqliteRepository;
 pub use feeds::FeedsSqliteRepository;
 pub use profiles::ProfilesSqliteRepository;
 pub use users::UsersSqliteRepository;
 
+mod collections;
 mod entries;
 mod feeds;
 mod profiles;
