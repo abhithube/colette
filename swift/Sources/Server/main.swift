dff8527bfe24ff2ccbca24fd386051f2b2b0d186
Abhi Thube <athube@abhithube.com>
2024-07-15 20:09:38 -0700

    Set up database, and defined initial migration

--- swift/Sources/Server/main.swift
+++ swift/Sources/Server/main.swift
@@ -1,6 +1,12 @@
+import Fluent
+import FluentPostgresDriver
 import OpenAPIRuntime
 import OpenAPIVapor
 import Vapor
+
+enum AppError: Error {
+  case runtimeError(String)
+}
 
 extension Components.Schemas.Login: Validatable {
   static func validations(_ validations: inout Validations) {
@@ -32,19 +38,34 @@ struct API: APIProtocol {
 
   func register(_ input: Operations.register.Input) async throws -> Operations.register.Output {
     switch input.body {
-      case .json(let body):
-        do {
-          try Components.Schemas.Register.validate(body)
-        } catch let error as ValidationsError {
-          return .unprocessableContent(.init(body: .json(.init(message: error.description))))
-        }
+    case .json(let body):
+      do {
+        try Components.Schemas.Register.validate(body)
+      } catch let error as ValidationsError {
+        return .unprocessableContent(.init(body: .json(.init(message: error.description))))
+      }
     }
 
     return .ok(.init(body: .json(.init(id: "", email: "", createdAt: .now, updatedAt: .now))))
   }
 }
 
+guard let databaseURL = Environment.get("DATABASE_URL") else {
+  throw AppError.runtimeError("\"DATABASE_URL\" not set")
+}
+
 let app = Vapor.Application()
+
+app.databases.use(
+  .postgres(
+    configuration: try .init(url: databaseURL)
+  ),
+  as: .psql
+)
+
+app.migrations.add(InitialMigration())
+
+try await app.autoMigrate().get()
 
 let transport = VaporTransport(routesBuilder: app)
 
