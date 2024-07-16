1fbc3576a90c7743c69f1dc516665690a58a3efa
Abhi Thube <athube@abhithube.com>
2024-07-16 13:18:47 -0700

    Added transaction for updating bookmark

--- rs/crates/sqlite/src/repositories/bookmarks.rs
+++ rs/crates/sqlite/src/repositories/bookmarks.rs
@@ -21,11 +21,11 @@ impl BookmarksSqliteRepository {
 #[async_trait]
 impl BookmarksRepository for BookmarksSqliteRepository {
     async fn find_many(&self, params: BookmarkFindManyParams) -> Result<Vec<Bookmark>, Error> {
-        let feeds = queries::bookmarks::select_many(&self.pool, (&params).into())
+        let bookmarks = queries::bookmarks::select_many(&self.pool, (&params).into())
             .await
             .map_err(|e| Error::Unknown(e.into()))?;
 
-        Ok(feeds)
+        Ok(bookmarks)
     }
 
     async fn update(
@@ -33,19 +33,27 @@ impl BookmarksRepository for BookmarksSqliteRepository {
         params: FindOneParams,
         data: BookmarkUpdateData,
     ) -> Result<Bookmark, Error> {
-        queries::bookmarks::update(&self.pool, (&params).into(), (&data).into())
+        let mut tx = self
+            .pool
+            .begin()
+            .await
+            .map_err(|e| Error::Unknown(e.into()))?;
+
+        queries::bookmarks::update(&mut *tx, (&params).into(), (&data).into())
             .await
             .map_err(|e| match e {
                 sqlx::Error::RowNotFound => Error::NotFound(params.id),
                 _ => Error::Unknown(e.into()),
             })?;
 
-        let bookmark = queries::bookmarks::select_by_id(&self.pool, (&params).into())
+        let bookmark = queries::bookmarks::select_by_id(&mut *tx, (&params).into())
             .await
             .map_err(|e| match e {
                 sqlx::Error::RowNotFound => Error::NotFound(params.id),
                 _ => Error::Unknown(e.into()),
             })?;
+
+        tx.commit().await.map_err(|e| Error::Unknown(e.into()))?;
 
         Ok(bookmark)
     }
