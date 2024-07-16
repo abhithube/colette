ad1247f0fa6bec4f209201293e3f9cda96701816
Abhi Thube <athube@abhithube.com>
2024-07-15 17:18:16 -0700

    Added request body validation

--- swift/Sources/Server/main.swift
+++ swift/Sources/Server/main.swift
@@ -2,9 +2,25 @@ import OpenAPIRuntime
 import OpenAPIVapor
 import Vapor
 
+extension Components.Schemas.Login: Validatable {
+  static func validations(_ validations: inout Validations) {
+    validations.add("email", as: String.self, is: .email)
+    validations.add("password", as: String.self, is: !.empty)
+  }
+}
+
 struct API: APIProtocol {
   func login(_ input: Operations.login.Input) async throws -> Operations.login.Output {
-    .ok(.init(body: .json(.init(id: "", email: "", createdAt: .now, updatedAt: .now))))
+    switch input.body {
+    case .json(let body):
+      do {
+        try Components.Schemas.Login.validate(body)
+      } catch let error as ValidationsError {
+        return .unprocessableContent(.init(body: .json(.init(message: error.description))))
+      }
+    }
+
+    return .ok(.init(body: .json(.init(id: "", email: "", createdAt: .now, updatedAt: .now))))
   }
 }
 
