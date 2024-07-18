000961ec1d33f4259ef499ad3d5b377d9aaf67b4
Abhi Thube <athube@abhithube.com>
2024-07-17 18:57:05 -0700

    Added bookmark scraper impl

---
+++ rs/crates/scraper/src/bookmarks/mod.rs
@@ -0,0 +1,67 @@
+use std::sync::Arc;
+
+use colette_core::{
+    bookmarks::{ExtractedBookmark, ProcessedBookmark},
+    utils::scraper::{Downloader, Error, Extractor, Postprocessor, Scraper},
+};
+use url::Url;
+
+use crate::PluginRegistry;
+
+pub struct BookmarkScraper {
+    registry: PluginRegistry<ExtractedBookmark, ProcessedBookmark>,
+    default_downloader: Arc<dyn Downloader + Send + Sync>,
+    default_extractor: Arc<dyn Extractor<ExtractedBookmark> + Send + Sync>,
+    default_postprocessor:
+        Arc<dyn Postprocessor<ExtractedBookmark, ProcessedBookmark> + Send + Sync>,
+}
+
+impl BookmarkScraper {
+    pub fn new(
+        registry: PluginRegistry<ExtractedBookmark, ProcessedBookmark>,
+        default_downloader: Arc<dyn Downloader + Send + Sync>,
+        default_extractor: Arc<dyn Extractor<ExtractedBookmark> + Send + Sync>,
+        default_postprocessor: Arc<
+            dyn Postprocessor<ExtractedBookmark, ProcessedBookmark> + Send + Sync,
+        >,
+    ) -> Self {
+        Self {
+            registry,
+            default_downloader,
+            default_extractor,
+            default_postprocessor,
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl Scraper<ProcessedBookmark> for BookmarkScraper {
+    async fn scrape(&self, url: &str) -> Result<ProcessedBookmark, Error> {
+        let parsed = Url::parse(url).map_err(|_| Error::Parse)?;
+        let host = parsed.host_str().ok_or(Error::Parse)?;
+
+        let downloader = self
+            .registry
+            .downloaders
+            .get(host)
+            .unwrap_or(&self.default_downloader);
+        let extractor = self
+            .registry
+            .extractors
+            .get(host)
+            .unwrap_or(&self.default_extractor);
+        let postprocessor = self
+            .registry
+            .postprocessors
+            .get(host)
+            .unwrap_or(&self.default_postprocessor);
+
+        let resp = downloader.download(url).await?;
+        let body = String::from_utf8_lossy(resp.body());
+
+        let extracted = extractor.extract(url, &body)?;
+        let processed = postprocessor.postprocess(url, extracted)?;
+
+        Ok(processed)
+    }
+}
--- rs/crates/scraper/src/lib.rs
+++ rs/crates/scraper/src/lib.rs
@@ -1,9 +1,11 @@
+pub use bookmarks::BookmarkScraper;
 pub use colette_core::feeds::{ExtractedEntry, ExtractedFeed, FeedExtractorOptions};
 pub use downloader::DefaultDownloader;
 pub use feeds::{AtomExtractorOptions, DefaultFeedExtractor, FeedScraper, RssExtractorOptions};
 pub use postprocessor::DefaultFeedPostprocessor;
 pub use registry::PluginRegistry;
 
+mod bookmarks;
 mod downloader;
 mod feeds;
 mod postprocessor;
