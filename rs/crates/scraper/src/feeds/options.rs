0bbe3b097e11928cef00f97c757b8b7afb15f5e5
Abhi Thube <athube@abhithube.com>
2024-07-17 20:22:10 -0700

    Added Media, DC, and iTunes feed extractor options

--- rs/crates/scraper/src/feeds/options.rs
+++ rs/crates/scraper/src/feeds/options.rs
@@ -16,22 +16,11 @@ impl Default for AtomExtractorOptions {
             feed_title_expr: &["/atom:feed/atom:title/text()"],
             feed_entries_expr: &["/atom:feed/atom:entry"],
             entry_link_expr: &["./atom:link/@href"],
-            entry_title_expr: &[
-                "./atom:title/text()",
-                "./media:group/media:title/text()",
-                "./media:title/text()",
-            ],
+            entry_title_expr: &["./atom:title/text()"],
             entry_published_expr: &["./atom:published/text()"],
-            entry_description_expr: &[
-                "./atom:content/text()",
-                "./media:group/media:description/text()",
-                "./media:description/text()",
-            ],
+            entry_description_expr: &["./atom:content/text()"],
             entry_author_expr: &["./atom:author/atom:name/text()"],
-            entry_thumbnail_expr: &[
-                "./media:group/media:thumbnail/@url",
-                "./media:thumbnail/@url",
-            ],
+            entry_thumbnail_expr: &[],
         })
     }
 }
@@ -55,8 +44,89 @@ impl Default for RssExtractorOptions {
             entry_title_expr: &["./title/text()"],
             entry_published_expr: &["./pubDate/text()"],
             entry_description_expr: &["./description/text()"],
-            entry_author_expr: &["./dc:creator/text()", "./author/text()"],
+            entry_author_expr: &["./author/text()"],
             entry_thumbnail_expr: &["./enclosure[starts-with(@type, 'image/')]/@url"],
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct MediaExtractorOptions(FeedExtractorOptions);
+
+impl MediaExtractorOptions {
+    pub fn inner(self) -> FeedExtractorOptions {
+        self.0
+    }
+}
+
+impl Default for MediaExtractorOptions {
+    fn default() -> Self {
+        Self(FeedExtractorOptions {
+            feed_link_expr: &[],
+            feed_title_expr: &[],
+            feed_entries_expr: &[],
+            entry_link_expr: &[],
+            entry_title_expr: &["./media:group/media:title/text()", "./media:title/text()"],
+            entry_published_expr: &[],
+            entry_description_expr: &[
+                "./media:group/media:description/text()",
+                "./media:description/text()",
+            ],
+            entry_author_expr: &[],
+            entry_thumbnail_expr: &[
+                "./media:group/media:thumbnail/@url",
+                "./media:thumbnail/@url",
+            ],
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct DublinCoreExtractorOptions(FeedExtractorOptions);
+
+impl DublinCoreExtractorOptions {
+    pub fn inner(self) -> FeedExtractorOptions {
+        self.0
+    }
+}
+
+impl Default for DublinCoreExtractorOptions {
+    fn default() -> Self {
+        Self(FeedExtractorOptions {
+            feed_link_expr: &[],
+            feed_title_expr: &["/rss/channel/dc:title/text()"],
+            feed_entries_expr: &[],
+            entry_link_expr: &[],
+            entry_title_expr: &["./dc:title/text()"],
+            entry_published_expr: &["./dc:date/text()"],
+            entry_description_expr: &["./dc:description/text()"],
+            entry_author_expr: &["./dc:creator/text()"],
+            entry_thumbnail_expr: &[],
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ITunesExtractorOptions(FeedExtractorOptions);
+
+impl ITunesExtractorOptions {
+    pub fn inner(self) -> FeedExtractorOptions {
+        self.0
+    }
+}
+
+impl Default for ITunesExtractorOptions {
+    fn default() -> Self {
+        Self(FeedExtractorOptions {
+            feed_link_expr: &[],
+            feed_title_expr: &["/rss/channel/itunes:title/text()"],
+            feed_entries_expr: &[],
+            entry_link_expr: &[],
+            entry_title_expr: &["./itunes:title/text()"],
+            entry_published_expr: &[],
+            entry_description_expr: &[],
+            entry_author_expr: &["/rss/channel/itunes:author/text()"],
+            entry_thumbnail_expr: &["./itunes:image/@href"],
         })
     }
 }
