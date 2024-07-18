0a71e6fbcfd6684dbfd1055498fd104a80c7521a
Abhi Thube <athube@abhithube.com>
2024-07-17 21:00:10 -0700

    Removed static lifetime from XPath util methods

--- rs/crates/scraper/src/utils.rs
+++ rs/crates/scraper/src/utils.rs
@@ -1,24 +1,19 @@
 use libxml::{tree::Node, xpath::Context};
 
 pub trait Xpath {
-    fn find_first_content(&mut self, exprs: &'static [&str], node: Option<&Node>)
-        -> Option<String>;
+    fn find_first_content(&mut self, exprs: &[&str], node: Option<&Node>) -> Option<String>;
 
-    fn find_nodes(&mut self, exprs: &'static [&str], node: Option<&Node>) -> Vec<Node>;
+    fn find_nodes(&mut self, exprs: &[&str], node: Option<&Node>) -> Vec<Node>;
 }
 
 impl Xpath for Context {
-    fn find_first_content(
-        &mut self,
-        exprs: &'static [&str],
-        node: Option<&Node>,
-    ) -> Option<String> {
+    fn find_first_content(&mut self, exprs: &[&str], node: Option<&Node>) -> Option<String> {
         exprs
             .iter()
             .find_map(|expr| self.findvalue(expr, node).ok())
     }
 
-    fn find_nodes(&mut self, exprs: &'static [&str], node: Option<&Node>) -> Vec<Node> {
+    fn find_nodes(&mut self, exprs: &[&str], node: Option<&Node>) -> Vec<Node> {
         exprs
             .iter()
             .find_map(|expr| self.findnodes(expr, node).ok())
