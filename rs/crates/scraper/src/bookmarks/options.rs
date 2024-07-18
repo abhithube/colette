c28c08ff69507120987591df0a2634b24c308277
Abhi Thube <athube@abhithube.com>
2024-07-18 09:11:06 -0700

    Fixed bookmark XPath expressions

--- rs/crates/scraper/src/bookmarks/options.rs
+++ rs/crates/scraper/src/bookmarks/options.rs
@@ -3,7 +3,7 @@ use colette_core::bookmarks::BookmarkExtractorOptions;
 pub fn base_extractor_options<'a>() -> BookmarkExtractorOptions<'a> {
     BookmarkExtractorOptions {
         title_expr: vec![
-            "/html/head/*[name='title']/@content",
+            "/html/head/*[@name='title']/@content",
             "/html/head/title/text()",
         ],
         published_expr: vec![],
@@ -14,30 +14,35 @@ pub fn base_extractor_options<'a>() -> BookmarkExtractorOptions<'a> {
 
 pub fn open_graph_extractor_options<'a>() -> BookmarkExtractorOptions<'a> {
     BookmarkExtractorOptions {
-        title_expr: vec!["/html/head/og:title/@content"],
+        title_expr: vec!["//*[@property='og:title']/@content"],
         published_expr: vec![],
         author_expr: vec![],
-        thumbnail_expr: vec!["/html/head/og:image/@content"],
+        thumbnail_expr: vec!["//*[@property='og:image']/@content"],
     }
 }
 
 pub fn twitter_extractor_options<'a>() -> BookmarkExtractorOptions<'a> {
     BookmarkExtractorOptions {
-        title_expr: vec!["/html/head/twitter:title/@content"],
+        title_expr: vec!["//*[@name='twitter:title']/@content"],
         published_expr: vec![],
         author_expr: vec![],
-        thumbnail_expr: vec!["/html/head/twitter:image/@content"],
+        thumbnail_expr: vec!["//*[@name='twitter:image']/@content"],
     }
 }
 
 pub fn microdata_extractor_options<'a>() -> BookmarkExtractorOptions<'a> {
     BookmarkExtractorOptions {
-        title_expr: vec!["/html/head/*[@itemscope and @itemtype='http://schema.org/VideoObject']/*[@itemprop='name']/@content"],
-        published_expr: vec![
-            "html/head/*[@itemscope and @itemtype='http://schema.org/VideoObject']/*[@itemprop='datePublished']/@content",
-            "/*[@itemscope and @itemtype='http://schema.org/VideoObject']/*[@itemprop='uploadDate']/@content"
+        title_expr: vec![
+            "//*[@itemtype='http://schema.org/VideoObject']/*[@itemprop='name']/@content",
         ],
-        author_expr: vec!["/html/head/*[@itemscope and @itemtype='http://schema.org/Person']/*[@itemprop='name']/@content"],
-        thumbnail_expr: vec!["/html/head/*[@itemscope and @itemtype='http://schema.org/ImageObject']/*[@itemprop='url']/@href", "/*[@itemprop='thumbnailUrl']/@href"],
+        published_expr: vec![
+            "//*[@itemtype='http://schema.org/VideoObject']/*[@itemprop='datePublished']/@content",
+            "//*[@itemtype='http://schema.org/VideoObject']/*[@itemprop='uploadDate']/@content",
+        ],
+        author_expr: vec!["//*[@itemtype='http://schema.org/Person']/*[@itemprop='name']/@content"],
+        thumbnail_expr: vec![
+            "//*[@itemtype='http://schema.org/ImageObject']/*[@itemprop='url']/@href",
+            "//*[@itemprop='thumbnailUrl']/@href",
+        ],
     }
 }
