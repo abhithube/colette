483aea9055be2517b3369d6c79dd50b6479c024f
Abhi Thube <athube@abhithube.com>
2024-07-19 23:14:34 -0700

    Added pool type re-exports

--- rs/crates/postgres/src/lib.rs
+++ rs/crates/postgres/src/lib.rs
@@ -2,11 +2,12 @@ pub use repositories::{
     BookmarksPostgresRepository, CollectionsPostgresRepository, EntriesPostgresRepository,
     FeedsPostgresRepository, ProfilesPostgresRepository, UsersPostgresRepository,
 };
+pub use sqlx::PgPool;
 
 mod queries;
 mod repositories;
 
-pub async fn create_database(url: &str) -> Result<sqlx::PgPool, sqlx::Error> {
+pub async fn create_database(url: &str) -> Result<PgPool, sqlx::Error> {
     let pool = sqlx::PgPool::connect(url).await?;
 
     sqlx::migrate!().run(&pool).await?;
--- rs/crates/sqlite/src/lib.rs
+++ rs/crates/sqlite/src/lib.rs
@@ -2,11 +2,12 @@ pub use repositories::{
     BookmarksSqliteRepository, CollectionsSqliteRepository, EntriesSqliteRepository,
     FeedsSqliteRepository, ProfilesSqliteRepository, UsersSqliteRepository,
 };
+pub use sqlx::SqlitePool;
 
 mod queries;
 mod repositories;
 
-pub async fn create_database(url: &str) -> Result<sqlx::SqlitePool, sqlx::Error> {
+pub async fn create_database(url: &str) -> Result<SqlitePool, sqlx::Error> {
     let pool = sqlx::SqlitePool::connect(url).await?;
 
     sqlx::migrate!().run(&pool).await?;
