972a47b8043152be798d3d2eb3aaa01389dc3686
Abhi Thube <athube@abhithube.com>
2024-07-20 19:18:31 -0700

    Switched to regex that is validated at compile time

--- rs/crates/plugins/src/youtube.rs
+++ rs/crates/plugins/src/youtube.rs
@@ -1,18 +1,14 @@
 use colette_core::utils::scraper::{DownloadError, DownloaderPlugin};
 use http::Request;
-use regex::Regex;
+use lazy_regex::regex_captures;
 use url::Url;
 
 pub const DOWNLOADER_PLUGIN: DownloaderPlugin = DownloaderPlugin::Callback(|url| {
-    let channel_regex =
-        Regex::new(r#"/channel/(UC[\w_-]+)"#).expect("failed to create channel regex");
-
     let mut parsed = Url::parse(url).map_err(|e| DownloadError(e.into()))?;
-    if let Some(captures) = channel_regex.captures(url) {
-        if let Some(m) = captures.get(1) {
-            parsed.set_path("feeds/videos.xml");
-            parsed.set_query(Some(&format!("channel_id={}", m.as_str())));
-        }
+
+    if let Some((_, channel_id)) = regex_captures!(r#"/channel/(UC[\w_-]+)"#, url) {
+        parsed.set_path("feeds/videos.xml");
+        parsed.set_query(Some(&format!("channel_id={}", channel_id)));
     }
 
     Request::get(parsed.as_ref())
