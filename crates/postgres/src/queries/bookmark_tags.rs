6c65b0d8fcc96373c7c2e5cd9392081c2f8570a1
Abhi Thube <athube@abhithube.com>
2024-07-25 23:32:36 -0700

    Updated queries to allow array operations using UNNEST

--- crates/postgres/src/queries/bookmark_tags.rs
+++ crates/postgres/src/queries/bookmark_tags.rs
@@ -1,13 +1,19 @@
 use colette_database::bookmark_tags::SelectParams;
 use sqlx::PgExecutor;
 
-pub async fn insert(ex: impl PgExecutor<'_>, params: SelectParams) -> Result<(), sqlx::Error> {
+pub async fn insert(ex: impl PgExecutor<'_>, params: Vec<SelectParams>) -> Result<(), sqlx::Error> {
+    let (bookmark_ids, tag_ids) = params
+        .iter()
+        .map(|e| (e.bookmark_id, e.tag_id))
+        .collect::<(Vec<_>, Vec<_>)>();
+
     sqlx::query!(
         "
 INSERT INTO bookmark_tags (bookmark_id, tag_id)
-VALUES ($1, $2)",
-        params.bookmark_id,
-        params.tag_id,
+SELECT * FROM UNNEST($1::UUID[], $2::UUID[])
+    ON CONFLICT (bookmark_id, tag_id) DO NOTHING",
+        &bookmark_ids,
+        &tag_ids,
     )
     .execute(ex)
     .await?;
@@ -15,14 +21,21 @@ VALUES ($1, $2)",
     Ok(())
 }
 
-pub async fn delete(ex: impl PgExecutor<'_>, params: SelectParams) -> Result<(), sqlx::Error> {
+pub async fn delete(ex: impl PgExecutor<'_>, params: Vec<SelectParams>) -> Result<(), sqlx::Error> {
+    let (bookmark_ids, tag_ids) = params
+        .iter()
+        .map(|e| (e.bookmark_id, e.tag_id))
+        .collect::<(Vec<_>, Vec<_>)>();
+
     sqlx::query!(
         "
 DELETE FROM bookmark_tags
- WHERE bookmark_id = $1
-   AND tag_id = $2",
-        params.bookmark_id,
-        params.tag_id,
+ WHERE (bookmark_id, tag_id) IN (
+       SELECT unnest($1::UUID[]) AS bookmark_id,
+              unnest($2::UUID[]) AS tag_id
+       )",
+        &bookmark_ids,
+        &tag_ids,
     )
     .execute(ex)
     .await?;
--- crates/postgres/src/queries/profile_feed_tags.rs
+++ crates/postgres/src/queries/profile_feed_tags.rs
@@ -1,13 +1,19 @@
 use colette_database::profile_feed_tags::SelectParams;
 use sqlx::PgExecutor;
 
-pub async fn insert(ex: impl PgExecutor<'_>, params: SelectParams) -> Result<(), sqlx::Error> {
+pub async fn insert(ex: impl PgExecutor<'_>, params: Vec<SelectParams>) -> Result<(), sqlx::Error> {
+    let (profile_feed_ids, tag_ids) = params
+        .iter()
+        .map(|e| (e.profile_feed_id, e.tag_id))
+        .collect::<(Vec<_>, Vec<_>)>();
+
     sqlx::query!(
         "
 INSERT INTO profile_feed_tags (profile_feed_id, tag_id)
-VALUES ($1, $2)",
-        params.profile_feed_id,
-        params.tag_id,
+SELECT * FROM UNNEST($1::UUID[], $2::UUID[])
+    ON CONFLICT (profile_feed_id, tag_id) DO NOTHING",
+        &profile_feed_ids,
+        &tag_ids,
     )
     .execute(ex)
     .await?;
@@ -15,14 +21,21 @@ VALUES ($1, $2)",
     Ok(())
 }
 
-pub async fn delete(ex: impl PgExecutor<'_>, params: SelectParams) -> Result<(), sqlx::Error> {
+pub async fn delete(ex: impl PgExecutor<'_>, params: Vec<SelectParams>) -> Result<(), sqlx::Error> {
+    let (profile_feed_ids, tag_ids) = params
+        .iter()
+        .map(|e| (e.profile_feed_id, e.tag_id))
+        .collect::<(Vec<_>, Vec<_>)>();
+
     sqlx::query!(
         "
 DELETE FROM profile_feed_tags
- WHERE profile_feed_id = $1
-   AND tag_id = $2",
-        params.profile_feed_id,
-        params.tag_id,
+ WHERE (profile_feed_id, tag_id) IN (
+       SELECT unnest($1::UUID[]) AS profile_feed_id,
+              unnest($2::UUID[]) AS tag_id
+       )",
+        &profile_feed_ids,
+        &tag_ids,
     )
     .execute(ex)
     .await?;
