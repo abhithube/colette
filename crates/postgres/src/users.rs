c97647d9cab23aaf29fa6ad1c14b3e1d1ef1c4c6
Abhi Thube <athube@abhithube.com>
2024-07-31 12:48:30 -0700

    Added registration and login database error handling

--- crates/postgres/src/users.rs
+++ crates/postgres/src/users.rs
@@ -1,5 +1,5 @@
 use colette_core::{
-    users::{Error, UsersCreateData, UsersFindOneParams, UsersRepository},
+    users::{Error, NotFoundError, UsersCreateData, UsersFindOneParams, UsersRepository},
     User,
 };
 
@@ -11,13 +11,19 @@ impl UsersRepository for PostgresRepository {
         sqlx::query_file_as!(User, "queries/users/find_one.sql", params.email)
             .fetch_one(&self.pool)
             .await
-            .map_err(|e| Error::Unknown(e.into()))
+            .map_err(|e| match e {
+                sqlx::Error::RowNotFound => Error::NotFound(NotFoundError::Email(params.email)),
+                _ => Error::Unknown(e.into()),
+            })
     }
 
     async fn create_user(&self, data: UsersCreateData) -> Result<User, Error> {
         sqlx::query_file_as!(User, "queries/users/insert.sql", data.email, data.password)
             .fetch_one(&self.pool)
             .await
-            .map_err(|e| Error::Unknown(e.into()))
+            .map_err(|e| match e {
+                sqlx::Error::Database(e) if e.is_unique_violation() => Error::Conflict(data.email),
+                _ => Error::Unknown(e.into()),
+            })
     }
 }
