8d7e7c3942bbe7588857cc10676d169396ea5bb0
Abhi Thube <athube@abhithube.com>
2024-08-04 00:26:14 -0700

    Implemented / and /stash routes for bookmarks

--- packages/web/src/routes/_private/bookmarks/index.tsx
+++ packages/web/src/routes/_private/bookmarks/index.tsx
@@ -1,5 +1,49 @@
+import { ensureInfiniteQueryData, listBookmarksOptions } from '@colette/query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react'
+import { BookmarkGrid } from '../-components/bookmark-grid'
 
 export const Route = createFileRoute('/_private/bookmarks/')({
-	component: () => <div>/bookmarks</div>,
+	loader: async ({ context }) => {
+		const options = listBookmarksOptions({}, context.profile.id, context.api)
+
+		await ensureInfiniteQueryData(context.queryClient, options as any)
+
+		return {
+			options,
+		}
+	},
+	component: Component,
 })
+
+function Component() {
+	const { options } = Route.useLoaderData()
+
+	const {
+		data: bookmarks,
+		hasNextPage,
+		fetchNextPage,
+	} = useInfiniteQuery(options)
+
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [])
+
+	if (!bookmarks) return
+
+	return (
+		<>
+			<header className="sticky top-0 w-full bg-background p-8">
+				<h1 className="truncate font-medium text-3xl">All Bookmarks</h1>
+			</header>
+			<main className="pb-8">
+				<BookmarkGrid
+					bookmarks={bookmarks.pages.flatMap((page) => page.data)}
+					hasMore={hasNextPage}
+					loadMore={fetchNextPage}
+				/>
+			</main>
+		</>
+	)
+}
--- packages/web/src/routes/_private/bookmarks/stash.tsx
+++ packages/web/src/routes/_private/bookmarks/stash.tsx
@@ -1,5 +1,53 @@
+import { ensureInfiniteQueryData, listBookmarksOptions } from '@colette/query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react'
+import { BookmarkGrid } from '../-components/bookmark-grid'
 
 export const Route = createFileRoute('/_private/bookmarks/stash')({
-	component: () => <div>/bookmarks/stash</div>,
+	loader: async ({ context }) => {
+		const options = listBookmarksOptions(
+			{ filterByTags: true, 'tag[]': [] },
+			context.profile.id,
+			context.api,
+		)
+
+		await ensureInfiniteQueryData(context.queryClient, options as any)
+
+		return {
+			options,
+		}
+	},
+	component: Component,
 })
+
+function Component() {
+	const { options } = Route.useLoaderData()
+
+	const {
+		data: bookmarks,
+		hasNextPage,
+		fetchNextPage,
+	} = useInfiniteQuery(options)
+
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [])
+
+	if (!bookmarks) return
+
+	return (
+		<>
+			<header className="sticky top-0 w-full bg-background p-8">
+				<h1 className="truncate font-medium text-3xl">Stash</h1>
+			</header>
+			<main className="pb-8">
+				<BookmarkGrid
+					bookmarks={bookmarks.pages.flatMap((page) => page.data)}
+					hasMore={hasNextPage}
+					loadMore={fetchNextPage}
+				/>
+			</main>
+		</>
+	)
+}
