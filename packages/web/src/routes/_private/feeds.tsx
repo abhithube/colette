5dfe7706bb43536ca572928a99b1377ef1200b6f
Abhi Thube <athube@abhithube.com>
2024-08-05 17:44:02 -0700

    Moved new feed/bookmark buttons to header

--- packages/web/src/routes/_private/feeds.tsx
+++ packages/web/src/routes/_private/feeds.tsx
@@ -9,7 +9,7 @@ import { Separator } from '@/components/ui/separator'
 import { listFeedsOptions } from '@colette/query'
 import { useQuery } from '@tanstack/react-query'
 import { Outlet, createFileRoute } from '@tanstack/react-router'
-import { History, Home, Plus } from 'lucide-react'
+import { History, Home, Plus, PlusCircle } from 'lucide-react'
 import { useState } from 'react'
 import { SidebarLink } from '../-components/sidebar-link'
 import { FeedItem } from './-components/feed-item'
@@ -40,8 +40,26 @@ function Component() {
 	return (
 		<div className="flex h-full w-full">
 			<ResizablePanelGroup direction="horizontal">
-				<ResizablePanel minSize={15} defaultSize={20} maxSize={30} collapsible>
-					<div className="space-y-1 p-4">
+				<ResizablePanel
+					className="space-y-4 pt-4"
+					minSize={15}
+					defaultSize={20}
+					maxSize={30}
+					collapsible
+				>
+					<div className="flex items-center justify-between px-4">
+						<h1 className="font-medium text-3xl">Feeds</h1>
+						<Dialog open={isOpen} onOpenChange={setOpen}>
+							<DialogTrigger asChild>
+								<Button className="space-x-2">
+									<span className="text-sm">New</span>
+									<PlusCircle className="h-4 w-4 shrink-0" />
+								</Button>
+							</DialogTrigger>
+							<SubscribeModal close={() => setOpen(false)} />
+						</Dialog>
+					</div>
+					<div className="space-y-1 px-4">
 						<SidebarLink to="/feeds" activeOptions={{ exact: true }}>
 							<Home className="h-4 w-4 shrink-0" />
 							<span className="grow truncate">All Feeds</span>
@@ -52,19 +70,14 @@ function Component() {
 						</SidebarLink>
 					</div>
 					<Separator />
-					<div className="h-full pt-4">
+					<div className="h-full">
 						<div className="flex h-8 items-center justify-between px-4">
 							<span className="grow font-semibold text-muted-foreground text-xs">
 								Feeds
 							</span>
-							<Dialog open={isOpen} onOpenChange={setOpen}>
-								<DialogTrigger asChild>
-									<Button className="h-8 w-8 justify-center" variant="ghost">
-										<Plus className="h-4 w-4 shrink-0" />
-									</Button>
-								</DialogTrigger>
-								<SubscribeModal close={() => setOpen(false)} />
-							</Dialog>
+							<Button className="h-8 w-8 justify-center" variant="ghost">
+								<Plus className="h-4 w-4 shrink-0" />
+							</Button>
 						</div>
 						<div className="mt-1 h-full space-y-1 overflow-y-auto px-4">
 							{feeds.data.length > 0 ? (
