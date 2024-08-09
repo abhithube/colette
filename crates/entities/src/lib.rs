fb690fd577d6b71af0dab7b8a69dbd6326b095d2
Abhi Thube <athube@abhithube.com>
2024-08-09 03:53:12 -0700

    Added entity to core struct mappers

--- crates/entities/src/lib.rs
+++ crates/entities/src/lib.rs
@@ -1,3 +1,26 @@
+use colette_core::{Profile, User};
 pub use generated::*;
 
 mod generated;
+
+impl From<profile::Model> for Profile {
+    fn from(value: profile::Model) -> Self {
+        Self {
+            id: value.id,
+            title: value.title,
+            image_url: value.image_url,
+            is_default: value.is_default,
+            user_id: value.user_id,
+        }
+    }
+}
+
+impl From<user::Model> for User {
+    fn from(value: user::Model) -> Self {
+        Self {
+            id: value.id,
+            email: value.email,
+            password: value.password,
+        }
+    }
+}
