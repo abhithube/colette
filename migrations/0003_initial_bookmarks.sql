7440f0308eabc8f13ad1ef428b2192ec10989d26
Abhi Thube <athube@abhithube.com>
2024-08-09 18:35:41 -0700

    Removed raw SQL migrations

--- migrations/0003_initial_bookmarks.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS bookmarks (
-  id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  link text NOT NULL UNIQUE,
-  title text NOT NULL,
-  thumbnail_url text,
-  published_at timestamptz,
-  author text
-);
--- migrations/0004_initial_profile_bookmarks.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS profile_bookmarks (
-  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid (),
-  profile_id UUID NOT NULL REFERENCES profiles (id) ON DELETE cascade,
-  bookmark_id int NOT NULL REFERENCES bookmarks (id) ON DELETE restrict,
-  UNIQUE (profile_id, bookmark_id)
-);
