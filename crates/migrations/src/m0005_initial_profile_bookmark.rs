134f1a3419838364d0ad84c49eb626cf3bd8f868
Abhi Thube <athube@abhithube.com>
2024-08-14 19:31:10 -0700

    Removed sort_index unique index

--- crates/migrations/src/m0005_initial_profile_bookmark.rs
+++ crates/migrations/src/m0005_initial_profile_bookmark.rs
@@ -62,25 +62,6 @@ impl MigrationTrait for Migration {
             )
             .await?;
 
-        let profile_bookmark_profile_id_sort_index_idx = format!(
-            "{profile_bookmark}_{profile_id}_{sort_index}_idx",
-            profile_bookmark = ProfileBookmark::Table.to_string(),
-            profile_id = ProfileBookmark::ProfileId.to_string(),
-            sort_index = ProfileBookmark::SortIndex.to_string()
-        );
-        manager
-            .create_index(
-                Index::create()
-                    .name(profile_bookmark_profile_id_sort_index_idx)
-                    .table(ProfileBookmark::Table)
-                    .if_not_exists()
-                    .col(ProfileBookmark::ProfileId)
-                    .col(ProfileBookmark::SortIndex)
-                    .unique()
-                    .to_owned(),
-            )
-            .await?;
-
         match manager.get_database_backend() {
             DatabaseBackend::Postgres => {
                 postgres::create_updated_at_trigger(manager, ProfileBookmark::Table.to_string())
