0e5fcebac1af84f0e26057b261bbe0d7932bda78
Abhi Thube <athube@abhithube.com>
2024-09-03 16:47:46 -0700

    Simplified custom feed plugins

--- crates/plugins/src/reddit.rs
+++ crates/plugins/src/reddit.rs
@@ -1,20 +1,18 @@
-use colette_scraper::{feed::FeedPlugin, DownloaderError, DownloaderPlugin};
+use colette_scraper::{feed::FeedPlugin, DownloaderError};
 use http::Request;
-
-const DOWNLOADER_PLUGIN: DownloaderPlugin = |url| {
-    if !url.path().contains(".rss") {
-        url.path_segments_mut().unwrap().pop_if_empty().push(".rss");
-    }
-
-    Request::get(url.as_ref())
-        .body(())
-        .map(|e| e.into_parts().0)
-        .map_err(|e| DownloaderError(e.into()))
-};
 
 pub fn new_reddit_feed_plugin() -> FeedPlugin<'static> {
     FeedPlugin {
-        downloader: Some(DOWNLOADER_PLUGIN),
+        downloader: Some(|url| {
+            if !url.path().contains(".rss") {
+                url.path_segments_mut().unwrap().pop_if_empty().push(".rss");
+            }
+
+            Request::get(url.as_ref())
+                .body(())
+                .map(|e| e.into_parts().0)
+                .map_err(|e| DownloaderError(e.into()))
+        }),
         ..Default::default()
     }
 }
--- crates/plugins/src/youtube.rs
+++ crates/plugins/src/youtube.rs
@@ -1,23 +1,22 @@
-use colette_scraper::{feed::FeedPlugin, DownloaderError, DownloaderPlugin};
+use colette_scraper::{feed::FeedPlugin, DownloaderError};
 use http::Request;
 use lazy_regex::regex_captures;
 
-const DOWNLOADER_PLUGIN: DownloaderPlugin = |url| {
-    if let Some((_, channel_id)) = regex_captures!(r#"/channel/(UC[\w_-]+)"#, url.clone().as_str())
-    {
-        url.set_path("feeds/videos.xml");
-        url.set_query(Some(&format!("channel_id={}", channel_id)));
-    }
-
-    Request::get(url.as_ref())
-        .body(())
-        .map(|e| e.into_parts().0)
-        .map_err(|e| DownloaderError(e.into()))
-};
-
 pub fn new_youtube_feed_plugin() -> FeedPlugin<'static> {
     FeedPlugin {
-        downloader: Some(DOWNLOADER_PLUGIN),
+        downloader: Some(|url| {
+            if let Some((_, channel_id)) =
+                regex_captures!(r#"/channel/(UC[\w_-]+)"#, url.clone().as_str())
+            {
+                url.set_path("feeds/videos.xml");
+                url.set_query(Some(&format!("channel_id={}", channel_id)));
+            }
+
+            Request::get(url.as_ref())
+                .body(())
+                .map(|e| e.into_parts().0)
+                .map_err(|e| DownloaderError(e.into()))
+        }),
         ..Default::default()
     }
 }
