7e4bfac5188e2a43734c6665c5110baa47b0493d
Abhi Thube <athube@abhithube.com>
2024-09-15 19:07:01 -0700

    Added Netscape backup routes

--- crates/api/src/backup.rs
+++ crates/api/src/backup.rs
@@ -31,6 +31,8 @@ impl BackupApi {
         OpenApiRouter::with_openapi(BackupApi::openapi())
             .routes(routes!(import_opml))
             .routes(routes!(export_opml))
+            .routes(routes!(import_netscape))
+            .routes(routes!(export_netscape))
     }
 }
 
@@ -58,7 +60,7 @@ pub async fn import_opml(
 #[utoipa::path(
     post,
     path = "/opml/export",
-    responses(ExportResponse),
+    responses(ExportOpmlResponse),
     operation_id = "exportOpml",
     description = "Export OPML feeds from profile",
     tag = BACKUPS_TAG
@@ -69,7 +71,47 @@ pub async fn export_opml(
     session: Session,
 ) -> Result<impl IntoResponse, Error> {
     match service.export_opml(session.profile_id).await {
-        Ok(data) => Ok(ExportResponse::Ok(data.into())),
+        Ok(data) => Ok(ExportOpmlResponse::Ok(data.into())),
+        _ => Err(Error::Unknown),
+    }
+}
+
+#[utoipa::path(
+    post,
+    path = "/netscape/import",
+    request_body = Vec<u8>,
+    responses(ImportResponse),
+    operation_id = "importNetscape",
+    description = "Import Netscape bookmarks into profile",
+    tag = BACKUPS_TAG
+)]
+#[axum::debug_handler]
+pub async fn import_netscape(
+    State(service): State<Arc<BackupService>>,
+    session: Session,
+    bytes: Bytes,
+) -> Result<ImportResponse, Error> {
+    match service.import_netscape(bytes, session.profile_id).await {
+        Ok(_) => Ok(ImportResponse::NoContent),
+        _ => Err(Error::Unknown),
+    }
+}
+
+#[utoipa::path(
+    post,
+    path = "/netscape/export",
+    responses(ExportNetscapeResponse),
+    operation_id = "exportNetscape",
+    description = "Export Netscape bookmarks from profile",
+    tag = BACKUPS_TAG
+)]
+#[axum::debug_handler]
+pub async fn export_netscape(
+    State(service): State<Arc<BackupService>>,
+    session: Session,
+) -> Result<impl IntoResponse, Error> {
+    match service.export_netscape(session.profile_id).await {
+        Ok(data) => Ok(ExportNetscapeResponse::Ok(data.into())),
         _ => Err(Error::Unknown),
     }
 }
@@ -89,7 +131,7 @@ impl IntoResponse for ImportResponse {
 }
 
 #[derive(Debug, utoipa::IntoResponses)]
-pub enum ExportResponse {
+pub enum ExportOpmlResponse {
     #[response(
         status = 200,
         description = "OPML file",
@@ -98,12 +140,35 @@ pub enum ExportResponse {
     Ok(Vec<u8>),
 }
 
-impl IntoResponse for ExportResponse {
+impl IntoResponse for ExportOpmlResponse {
     fn into_response(self) -> Response {
         match self {
             Self::Ok(data) => {
                 let mut headers = HeaderMap::new();
                 headers.insert("Content-Type", HeaderValue::from_static("application/xml"));
+
+                (headers, data).into_response()
+            }
+        }
+    }
+}
+
+#[derive(Debug, utoipa::IntoResponses)]
+pub enum ExportNetscapeResponse {
+    #[response(
+        status = 200,
+        description = "Netscape file",
+        content_type = "text/html"
+    )]
+    Ok(Vec<u8>),
+}
+
+impl IntoResponse for ExportNetscapeResponse {
+    fn into_response(self) -> Response {
+        match self {
+            Self::Ok(data) => {
+                let mut headers = HeaderMap::new();
+                headers.insert("Content-Type", HeaderValue::from_static("text/html"));
 
                 (headers, data).into_response()
             }
