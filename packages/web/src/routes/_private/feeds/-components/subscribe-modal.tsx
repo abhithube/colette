a5a67800db0f9ccd6ec25137913f06a2d8d1fa45
Abhi Thube <athube@abhithube.com>
2024-09-16 22:06:50 -0700

    Styled subscribe modal

--- packages/web/src/routes/_private/feeds/-components/subscribe-modal.tsx
+++ packages/web/src/routes/_private/feeds/-components/subscribe-modal.tsx
@@ -1,25 +1,20 @@
-import { Icon } from '@/components/icon'
-import { Button } from '@/components/ui/button'
 import {
-  Form,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { Dialog, IconButton } from '@colette/components'
-import {
-  BadGatewayError,
-  FeedCreate,
-  UnprocessableContentError,
-} from '@colette/core'
+  Button,
+  Dialog,
+  Field,
+  Fieldset,
+  Flex,
+  IconButton,
+  Switch,
+  VStack,
+} from '@colette/components'
 import { createFeedOptions } from '@colette/query'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from '@tanstack/react-form'
 import { useMutation } from '@tanstack/react-query'
 import { useNavigate } from '@tanstack/react-router'
-import { Loader2, X } from 'lucide-react'
-import { useForm } from 'react-hook-form'
+import { zodValidator } from '@tanstack/zod-form-adapter'
+import { X } from 'lucide-react'
+import { z } from 'zod'
 import { Route } from '../../feeds'
 
 type Props = {
@@ -29,11 +24,16 @@ type Props = {
 export function SubscribeModal({ close }: Props) {
   const context = Route.useRouteContext()
 
-  const form = useForm<FeedCreate>({
-    resolver: zodResolver(FeedCreate),
+  const {
+    Field: TField,
+    handleSubmit,
+    reset,
+  } = useForm({
     defaultValues: {
       url: '',
+      pinned: false,
     },
+    onSubmit: ({ value }) => createFeed(value),
   })
 
   const navigate = useNavigate()
@@ -42,7 +42,7 @@ export function SubscribeModal({ close }: Props) {
     createFeedOptions(
       {
         onSuccess: async (data) => {
-          form.reset()
+          reset()
           close()
 
           await context.queryClient.invalidateQueries({
@@ -56,54 +56,78 @@ export function SubscribeModal({ close }: Props) {
             },
           })
         },
-        onError: (error) => {
-          if (error instanceof UnprocessableContentError) {
-            form.setError('root', {
-              message: error.message,
-            })
-          } else if (error instanceof BadGatewayError) {
-            form.setError('root', {
-              message: error.message,
-            })
-          }
-        },
       },
       context.api,
     ),
   )
 
   return (
-    <Dialog.Content>
-      <Form {...form}>
-        <form
-          className="space-y-4"
-          onSubmit={form.handleSubmit((data) => createFeed(data))}
-        >
-          <Dialog.Title>Add Feed</Dialog.Title>
-          <Dialog.Description>
-            Subscribe to a RSS or Atom feed and receive the latest updates.
-          </Dialog.Description>
-          <FormField
-            control={form.control}
+    <Dialog.Content p={6}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          handleSubmit()
+        }}
+      >
+        <Dialog.Title>Add Feed</Dialog.Title>
+        <Dialog.Description>
+          Subscribe to a RSS or Atom feed and receive the latest updates.
+        </Dialog.Description>
+        <VStack alignItems="stretch" spaceY={4} mt={4}>
+          <TField
             name="url"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>URL</FormLabel>
-                <Input {...field} />
-                <FormDescription>URL of the RSS or Atom Feed</FormDescription>
-              </FormItem>
+            validatorAdapter={zodValidator()}
+            validators={{
+              onBlur: z.string().url('Please enter a valid URL'),
+            }}
+          >
+            {({ state, handleChange, handleBlur }) => (
+              <Field.Root
+                defaultValue={state.value}
+                invalid={state.meta.errors.length > 0}
+              >
+                <Field.Label>URL</Field.Label>
+                <Field.Input
+                  placeholder="https://www.website.com"
+                  onChange={(e) => handleChange(e.target.value)}
+                  onBlur={handleBlur}
+                />
+                <Field.HelperText>URL of the RSS or Atom Feed</Field.HelperText>
+                <Field.ErrorText>
+                  {state.meta.errors[0]?.toString()}
+                </Field.ErrorText>
+              </Field.Root>
             )}
-          />
-          <Button disabled={isPending}>
-            {isPending && (
-              <Icon className="mr-2 animate-spin" value={Loader2} />
+          </TField>
+          <TField
+            name="pinned"
+            validatorAdapter={zodValidator()}
+            validators={{
+              onBlur: z.boolean(),
+            }}
+          >
+            {({ handleChange, handleBlur }) => (
+              <Fieldset.Root paddingBlock={0} borderTop="none">
+                <Fieldset.Legend>Pinned</Fieldset.Legend>
+                <Fieldset.HelperText>
+                  Should the feed be pinned to the sidebar?
+                </Fieldset.HelperText>
+                <Field.Root>
+                  <Switch
+                    onCheckedChange={(details) => handleChange(details.checked)}
+                    onBlur={handleBlur}
+                  />
+                </Field.Root>
+              </Fieldset.Root>
             )}
-            Submit
-          </Button>
-        </form>
-      </Form>
+          </TField>
+          <Flex justify="end">
+            <Button loading={isPending}>Submit</Button>
+          </Flex>
+        </VStack>
+      </form>
       <Dialog.CloseTrigger asChild position="absolute" top="2" right="2">
-        <IconButton aria-label="Close Dialog" variant="ghost" size="sm">
+        <IconButton variant="ghost" size="sm">
           <X />
         </IconButton>
       </Dialog.CloseTrigger>
