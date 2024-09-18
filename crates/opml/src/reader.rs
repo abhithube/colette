0823aca7100cab716d2f915a25ebd9b613e5b01b
Abhi Thube <athube@abhithube.com>
2024-09-18 11:03:18 -0700

    Added check for self-closing outline tags

--- crates/opml/src/reader.rs
+++ crates/opml/src/reader.rs
@@ -1,6 +1,9 @@
 use std::io::BufRead;
 
-use quick_xml::{events::Event, Reader};
+use quick_xml::{
+    events::{BytesStart, Event},
+    Reader,
+};
 
 use crate::{Opml, Outline};
 
@@ -33,27 +36,23 @@ pub fn from_reader<R: BufRead>(reader: R) -> Result<Opml, anyhow::Error> {
                 }
                 b"title" => current = Some(OpmlField::Title),
                 b"outline" => {
-                    let mut outline = Outline::default();
-
-                    for attribute in e.attributes() {
-                        let attribute = attribute?;
-
-                        let value = attribute.decode_and_unescape_value(reader.decoder())?;
-
-                        match attribute.key.local_name().into_inner() {
-                            b"type" => outline.r#type = Some(value.parse()?),
-                            b"text" => outline.text = value.into_owned(),
-                            b"xmlUrl" => outline.xml_url = Some(value.into_owned()),
-                            b"title" => outline.title = Some(value.into_owned()),
-                            b"htmlUrl" => outline.html_url = Some(value.into_owned()),
-                            _ => {}
-                        }
-                    }
+                    let outline = handle_outline(&reader, e)?;
 
                     stack.push(outline);
                 }
                 _ => {}
             },
+            Ok(Event::Empty(e)) => {
+                if e.name().as_ref() == b"outline" {
+                    let outline = handle_outline(&reader, e)?;
+
+                    if let Some(parent) = stack.last_mut() {
+                        parent.outline.get_or_insert_with(Vec::new).push(outline);
+                    } else {
+                        opml.body.outlines.push(outline);
+                    }
+                }
+            }
             Ok(Event::Text(e)) => {
                 if let Some(OpmlField::Title) = current {
                     opml.head.title = e.unescape()?.into_owned();
@@ -80,4 +79,27 @@ pub fn from_reader<R: BufRead>(reader: R) -> Result<Opml, anyhow::Error> {
     }
 
     Ok(opml)
+}
+
+fn handle_outline<R: BufRead>(
+    reader: &Reader<R>,
+    tag: BytesStart,
+) -> Result<Outline, anyhow::Error> {
+    let mut outline = Outline::default();
+
+    for attribute in tag.attributes() {
+        let attribute = attribute?;
+
+        let value = attribute.decode_and_unescape_value(reader.decoder())?;
+        match attribute.key.local_name().into_inner() {
+            b"type" => outline.r#type = Some(value.parse()?),
+            b"text" => outline.text = value.into_owned(),
+            b"xmlUrl" => outline.xml_url = Some(value.into_owned()),
+            b"title" => outline.title = Some(value.into_owned()),
+            b"htmlUrl" => outline.html_url = Some(value.into_owned()),
+            _ => {}
+        }
+    }
+
+    Ok(outline)
 }
