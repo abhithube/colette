4871283fb8de0189f81bbb898c8aefcc2a0e2491
Abhi Thube <athube@abhithube.com>
2024-09-24 21:44:23 -0700

    Added SQLite database initialization

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,14 +1,32 @@
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::fs;
+
+use colette_migration::{Migrator, MigratorTrait};
+use sea_orm::{ConnectOptions, Database};
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            tauri::async_runtime::block_on(async move {
+                let mut path = app.path().app_data_dir()?;
+                if !path.exists() {
+                    fs::create_dir_all(&path)?;
+                }
+                path = path.join("sqlite.db");
+
+                let database_url = format!("sqlite://{}?mode=rwc", path.to_string_lossy());
+
+                let mut opts = ConnectOptions::new(database_url);
+                opts.max_connections(100);
+
+                let db = Database::connect(opts).await?;
+                Migrator::up(&db, None).await?;
+
+                Ok(())
+            })
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
