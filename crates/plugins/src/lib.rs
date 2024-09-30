37c3adb681cd5c968ed58c311e4e78e20ac5c213
Abhi Thube <athube@abhithube.com>
2024-09-30 11:22:48 -0700

    Removed unnecessary explicit type annotations

--- crates/plugins/src/lib.rs
+++ crates/plugins/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use colette_scraper::{BookmarkPluginRegistry, BookmarkScraper, FeedPluginRegistry, FeedScraper};
+use colette_scraper::{BookmarkPluginRegistry, FeedPluginRegistry};
 #[allow(unused_imports)]
 use custom::*;
 
@@ -9,17 +9,12 @@ mod reddit;
 mod youtube;
 
 pub fn register_feed_plugins() -> FeedPluginRegistry {
-    let plugins: HashMap<&str, Box<dyn FeedScraper>> = HashMap::from([
+    FeedPluginRegistry::new(HashMap::from([
         ("www.reddit.com", reddit::feed()),
         ("www.youtube.com", youtube::create()),
-    ]);
-
-    FeedPluginRegistry::new(plugins)
+    ]))
 }
 
 pub fn register_bookmark_plugins() -> BookmarkPluginRegistry {
-    let plugins: HashMap<&str, Box<dyn BookmarkScraper>> =
-        HashMap::from([("www.reddit.com", reddit::bookmark())]);
-
-    BookmarkPluginRegistry::new(plugins)
+    BookmarkPluginRegistry::new(HashMap::from([("www.reddit.com", reddit::bookmark())]))
 }
