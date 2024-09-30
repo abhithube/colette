37097fcb1d7eaf15abc4763a6fc5289a4a5aadf0
Abhi Thube <athube@abhithube.com>
2024-09-30 11:19:48 -0700

    Added Reddit bookmark parser

--- crates/plugins/src/lib.rs
+++ crates/plugins/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use colette_scraper::{BookmarkPluginRegistry, FeedPluginRegistry, FeedScraper};
+use colette_scraper::{BookmarkPluginRegistry, BookmarkScraper, FeedPluginRegistry, FeedScraper};
 #[allow(unused_imports)]
 use custom::*;
 
@@ -10,7 +10,7 @@ mod youtube;
 
 pub fn register_feed_plugins() -> FeedPluginRegistry {
     let plugins: HashMap<&str, Box<dyn FeedScraper>> = HashMap::from([
-        ("www.reddit.com", reddit::create()),
+        ("www.reddit.com", reddit::feed()),
         ("www.youtube.com", youtube::create()),
     ]);
 
@@ -18,5 +18,8 @@ pub fn register_feed_plugins() -> FeedPluginRegistry {
 }
 
 pub fn register_bookmark_plugins() -> BookmarkPluginRegistry {
-    BookmarkPluginRegistry::default()
+    let plugins: HashMap<&str, Box<dyn BookmarkScraper>> =
+        HashMap::from([("www.reddit.com", reddit::bookmark())]);
+
+    BookmarkPluginRegistry::new(plugins)
 }
--- crates/plugins/src/reddit.rs
+++ crates/plugins/src/reddit.rs
@@ -1,10 +1,14 @@
-use colette_scraper::FeedScraper;
+use colette_scraper::{
+    utils::{ExtractorQuery, Node},
+    BookmarkExtractorOptions, BookmarkScraper, FeedScraper,
+};
 use http::{header, request::Builder, Request};
+use scraper::Selector;
 use url::Url;
 
 pub struct RedditPlugin;
 
-pub fn create() -> Box<dyn FeedScraper> {
+pub fn feed() -> Box<dyn FeedScraper> {
     Box::new(RedditPlugin)
 }
 
@@ -16,5 +20,32 @@ impl FeedScraper for RedditPlugin {
 
         Request::get(url.as_ref())
             .header(header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36")
+    }
+}
+
+pub fn bookmark() -> Box<dyn BookmarkScraper> {
+    Box::new(RedditPlugin)
+}
+
+impl BookmarkScraper for RedditPlugin {
+    fn before_extract(&self) -> Option<BookmarkExtractorOptions> {
+        Some(BookmarkExtractorOptions {
+            title_queries: vec![ExtractorQuery {
+                selector: Selector::parse("shreddit-post").unwrap(),
+                node: Node::Attr("post-title"),
+            }],
+            thumbnail_queries: vec![ExtractorQuery {
+                selector: Selector::parse(".preview-img").unwrap(),
+                node: Node::Attr("src"),
+            }],
+            published_queries: vec![ExtractorQuery {
+                selector: Selector::parse("shreddit-post").unwrap(),
+                node: Node::Attr("created-timestamp"),
+            }],
+            author_queries: vec![ExtractorQuery {
+                selector: Selector::parse("shreddit-post").unwrap(),
+                node: Node::Attr("author"),
+            }],
+        })
     }
 }
