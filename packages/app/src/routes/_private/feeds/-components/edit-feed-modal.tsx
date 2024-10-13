72f425b8996233b6d7e25a7652c6b015db3249a1
Abhi Thube <athube@abhithube.com>
2024-10-12 19:31:45 -0700

    Extracted tag selector into component

--- packages/app/src/routes/_private/feeds/-components/edit-feed-modal.tsx
+++ packages/app/src/routes/_private/feeds/-components/edit-feed-modal.tsx
@@ -2,25 +2,21 @@ import type { Feed } from '@colette/core'
 import { listTagsOptions, updateFeedOptions } from '@colette/query'
 import {
   Button,
-  Combobox,
   Dialog,
   Field,
   Fieldset,
   Flex,
-  HStack,
-  Icon,
   IconButton,
   Switch,
-  TagsInput,
   VStack,
-  createListCollection,
 } from '@colette/ui'
 import { useForm } from '@tanstack/react-form'
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { zodValidator } from '@tanstack/zod-form-adapter'
-import { Plus, X } from 'lucide-react'
+import { X } from 'lucide-react'
 import { useEffect } from 'react'
 import { z } from 'zod'
+import { TagSelector } from '../../../../components/tag-selector'
 import { Route } from '../../../_private'
 
 type Props = {
@@ -114,12 +110,6 @@ export function EditFeedModal({ feed, close }: Props) {
 
   if (!tags) return
 
-  const collection = createListCollection({
-    items: tags.data,
-    itemToString: (item) => item.title,
-    itemToValue: (item) => item.title,
-  })
-
   return (
     <Dialog.Content maxW="md" p={6}>
       <form
@@ -173,98 +163,11 @@ export function EditFeedModal({ feed, close }: Props) {
           </form.Field>
           <form.Field name="tags">
             {({ state, handleChange }) => (
-              <Combobox.Root
-                asChild
-                collection={collection}
-                multiple
-                allowCustomValue
-                openOnClick
-                value={state.value}
-                onValueChange={(details) =>
-                  handleChange(details.value.toSorted())
-                }
-              >
-                <TagsInput.Root
-                  value={state.value}
-                  onValueChange={(details) =>
-                    handleChange(details.value.toSorted())
-                  }
-                >
-                  <Combobox.Label>Tags</Combobox.Label>
-                  <TagsInput.Context>
-                    {({ value, inputValue, clearInputValue }) => (
-                      <>
-                        <Combobox.Control asChild>
-                          <TagsInput.Control>
-                            {value.map((item) => (
-                              <TagsInput.Item
-                                key={item}
-                                index={item}
-                                value={item}
-                              >
-                                <TagsInput.ItemPreview>
-                                  <TagsInput.ItemText>
-                                    {item}
-                                  </TagsInput.ItemText>
-                                  <TagsInput.ItemDeleteTrigger asChild>
-                                    <IconButton variant="link" size="xs">
-                                      <X />
-                                    </IconButton>
-                                  </TagsInput.ItemDeleteTrigger>
-                                </TagsInput.ItemPreview>
-                                <TagsInput.ItemInput />
-                                <TagsInput.HiddenInput />
-                              </TagsInput.Item>
-                            ))}
-                            <Combobox.Input placeholder="Add tag..." asChild>
-                              <TagsInput.Input />
-                            </Combobox.Input>
-                          </TagsInput.Control>
-                        </Combobox.Control>
-                        <Combobox.Positioner>
-                          <Combobox.Content>
-                            <Combobox.ItemGroup>
-                              <Combobox.ItemGroupLabel>
-                                Tags
-                              </Combobox.ItemGroupLabel>
-                              {collection.items
-                                .filter((tag) => !value.includes(tag.title))
-                                .filter((tag) =>
-                                  inputValue.length > 0
-                                    ? tag.title
-                                        .toLowerCase()
-                                        .includes(inputValue.toLowerCase())
-                                    : true,
-                                )
-                                .map((tag) => (
-                                  <Combobox.Item key={tag.title} item={tag}>
-                                    {tag.title}
-                                  </Combobox.Item>
-                                ))}
-                              {inputValue.length > 0 &&
-                                !collection.has(inputValue) &&
-                                !value.includes(inputValue) && (
-                                  <Combobox.Item
-                                    key={inputValue}
-                                    item={{ id: '', title: inputValue }}
-                                    onClick={clearInputValue}
-                                  >
-                                    <HStack>
-                                      <Icon>
-                                        <Plus />
-                                      </Icon>
-                                      Create new tag
-                                    </HStack>
-                                  </Combobox.Item>
-                                )}
-                            </Combobox.ItemGroup>
-                          </Combobox.Content>
-                        </Combobox.Positioner>
-                      </>
-                    )}
-                  </TagsInput.Context>
-                </TagsInput.Root>
-              </Combobox.Root>
+              <TagSelector
+                tags={tags.data}
+                state={state}
+                handleChange={handleChange}
+              />
             )}
           </form.Field>
           <Flex justify="end">
