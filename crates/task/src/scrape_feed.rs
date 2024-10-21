91170fc2fc407b9d426d9494d343eeb81639874c
Abhi Thube <athube@abhithube.com>
2024-10-21 00:52:14 -0700

    Added error type to worker function

--- crates/task/src/scrape_feed.rs
+++ crates/task/src/scrape_feed.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use apalis::prelude::{Data, Job};
-use colette_core::scraper::{FeedCreate, ScraperService};
+use colette_core::scraper::{self, FeedCreate, ScraperService};
 use url::Url;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
@@ -13,6 +13,6 @@ impl Job for Args {
     const NAME: &'static str = "apalis::ScrapeFeed";
 }
 
-pub async fn run(args: Args, service: Data<Arc<ScraperService>>) {
-    let _ = service.scrape_feed(FeedCreate { url: args.url }).await;
+pub async fn run(args: Args, service: Data<Arc<ScraperService>>) -> Result<(), scraper::Error> {
+    service.scrape_feed(FeedCreate { url: args.url }).await
 }
