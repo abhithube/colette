54c1c083e53c380ee13f11378e879d68520f3951
Abhi Thube <athube@abhithube.com>
2024-10-22 15:05:48 -0700

    Added import feeds task

---
+++ crates/task/src/import_feeds.rs
@@ -0,0 +1,53 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+};
+
+use colette_core::scraper;
+use tower::Service;
+use url::Url;
+
+use crate::{scrape_feed, TaskQueue};
+
+#[derive(Debug, Clone)]
+pub struct Data {
+    pub urls: Vec<Url>,
+}
+
+#[derive(Clone)]
+pub struct Task {
+    scrape_feed_queue: Arc<TaskQueue<scrape_feed::Data>>,
+}
+
+impl Task {
+    pub fn new(scrape_feed_queue: Arc<TaskQueue<scrape_feed::Data>>) -> Self {
+        Self { scrape_feed_queue }
+    }
+}
+
+impl Service<Data> for Task {
+    type Response = ();
+    type Error = scraper::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Data) -> Self::Future {
+        let scrape_feed_queue = self.scrape_feed_queue.clone();
+
+        Box::pin(async move {
+            for url in req.urls {
+                scrape_feed_queue
+                    .push(scrape_feed::Data { url })
+                    .await
+                    .unwrap();
+            }
+
+            Ok(())
+        })
+    }
+}
