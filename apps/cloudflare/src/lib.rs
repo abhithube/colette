a5fc5b586c4e60fe6355dc31cc54928c11982b9f
Abhi Thube <athube@abhithube.com>
2024-11-15 20:34:14 -0800

    Added Cloudflare Workers app

---
+++ apps/cloudflare/src/lib.rs
@@ -0,0 +1,17 @@
+use axum::{body::Body, http::Response, routing::get, Router};
+use tower::Service;
+use worker::{Context, Env, HttpRequest};
+
+#[worker::event(fetch)]
+async fn fetch(req: HttpRequest, _env: Env, _ctx: Context) -> worker::Result<Response<Body>> {
+    console_error_panic_hook::set_once();
+
+    let mut router = Router::new().route("/", get(root));
+    let resp = router.call(req).await?;
+
+    Ok(resp)
+}
+
+pub async fn root() -> &'static str {
+    "Hello world!"
+}
