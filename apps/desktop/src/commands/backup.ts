8c2e40d044783ec2077a54afe5faf16a14074f87
Abhi Thube <athube@abhithube.com>
2024-11-27 05:38:13 -0800

    Fixed file to bytes conversion

--- apps/desktop/src/commands/backup.ts
+++ apps/desktop/src/commands/backup.ts
@@ -3,6 +3,6 @@ import { invoke } from '@tauri-apps/api/core'
 
 export class BackupCommands implements BackupAPI {
   async import(data: File): Promise<void> {
-    return invoke('import_opml', { data: await data.bytes() }).then()
+    return invoke('import_opml', { data: new Uint8Array(await data.arrayBuffer()) }).then()
   }
 }
