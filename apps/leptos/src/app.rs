710ab3d3f1fe850021951a128def37c652360ab3
Abhi Thube <athube@abhithube.com>
2024-12-01 18:09:35 -0800

    Added login form with server function

--- apps/leptos/src/app.rs
+++ apps/leptos/src/app.rs
@@ -1,3 +1,4 @@
+use colette_api::auth;
 use leptos::prelude::*;
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
@@ -44,11 +45,43 @@ pub fn App() -> impl IntoView {
 
 #[component]
 fn HomePage() -> impl IntoView {
-    let count = RwSignal::new(0);
-    let on_click = move |_| *count.write() += 1;
+    view! {
+        <div class="flex items-center justify-center">
+            <div class="w-[400px]">
+                <LoginForm />
+            </div>
+        </div>
+    }
+}
+
+#[component]
+fn LoginForm() -> impl IntoView {
+    let submit = ServerAction::<Login>::new();
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <ActionForm action=submit>
+            <div class="flex flex-col">
+                <input type="text" name="data[email]" />
+                <input type="password" name="data[password]" />
+                <input type="submit" value="Submit" />
+            </div>
+        </ActionForm>
     }
+}
+
+#[server]
+pub async fn login(data: auth::Login) -> Result<auth::LoginResponse, ServerFnError> {
+    use crate::AppState;
+    use axum::{extract::State, Json};
+    use colette_core::auth::AuthService;
+    use leptos_axum::extract_with_state;
+
+    let state = expect_context::<AppState>();
+    let session: tower_sessions::Session = extract_with_state(&state.api_state).await?;
+    let State(state): State<auth::AuthState> = extract_with_state(&state.api_state).await?;
+    let state: State<AuthService> = extract_with_state(&state).await?;
+
+    let resp = auth::login(state, session, Json(data)).await?;
+
+    Ok(resp)
 }
