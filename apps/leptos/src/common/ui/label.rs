c861b02de4a6f060499c3cca8a3a86b66a4095e4
Abhi Thube <athube@abhithube.com>
2024-12-04 02:48:54 -0800

    Fixed label base class

--- apps/leptos/src/common/ui/label.rs
+++ apps/leptos/src/common/ui/label.rs
@@ -3,7 +3,9 @@ use tailwind_fuse::*;
 
 #[component]
 pub fn Label(#[prop(into, optional)] class: Signal<String>, children: Children) -> impl IntoView {
-    let class = move || tw_merge!("apps/leptos/src/common/ui/input.rs", class.get());
+    let class = move || {
+        tw_merge!("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", class.get())
+    };
 
     view! { <label class=class>{children()}</label> }
 }
