258a85d6d88f81fa7d9c07f978cac0358d016a5a
Abhi Thube <athube@abhithube.com>
2024-12-28 23:06:55 -0800

    Set up Tanstack Query provider

--- apps/solid/src/index.tsx
+++ apps/solid/src/index.tsx
@@ -1,7 +1,10 @@
 /* @refresh reload */
+import { HttpAPI } from '@colette/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
 import { render } from 'solid-js/web'
 import App from './App'
 import './index.css'
+import { APIProvider } from './lib/api-context'
 
 const root = document.getElementById('root')
 
@@ -11,4 +14,22 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   )
 }
 
-render(() => <App />, root!)
+render(
+  () => (
+    <QueryClientProvider client={new QueryClient()}>
+      <APIProvider
+        api={
+          new HttpAPI({
+            baseUrl: import.meta.env.DEV
+              ? import.meta.env.VITE_BACKEND_URL
+              : '/api/v1',
+            credentials: 'include',
+          })
+        }
+      >
+        <App />
+      </APIProvider>
+    </QueryClientProvider>
+  ),
+  root!,
+)
