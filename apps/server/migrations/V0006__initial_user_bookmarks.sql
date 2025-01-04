6dabc70b23d53a4a41a6e74f94bd203cd5dc5978
Abhi Thube <athube@abhithube.com>
2025-01-03 19:25:38 -0800

    Added collections table

--- apps/server/migrations/V0006__initial_user_bookmarks.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_bookmarks (
-  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-  user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-  bookmark_id INTEGER NOT NULL REFERENCES bookmarks (id) ON DELETE RESTRICT,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-  UNIQUE (user_id, bookmark_id)
-);
