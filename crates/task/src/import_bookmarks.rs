f26de7cbac971b55cea410e5d47eef625e649b51
Abhi Thube <athube@abhithube.com>
2025-01-28 20:35:05 -0800

    Fixed RedisStorage type

--- crates/task/src/import_bookmarks.rs
+++ crates/task/src/import_bookmarks.rs
@@ -2,7 +2,6 @@ use std::sync::Arc;
 
 use apalis::prelude::{Data, Storage};
 use apalis_redis::RedisStorage;
-use redis::aio::MultiplexedConnection;
 use tokio::sync::Mutex;
 use url::Url;
 
@@ -21,7 +20,7 @@ impl Job {
 
 pub async fn run(
     job: Job,
-    data: Data<Arc<Mutex<RedisStorage<scrape_bookmark::Job, MultiplexedConnection>>>>,
+    data: Data<Arc<Mutex<RedisStorage<scrape_bookmark::Job>>>>,
 ) -> Result<(), apalis::prelude::Error> {
     tracing::debug!("Importing {} bookmarks", job.urls.len());
 
--- crates/task/src/import_feeds.rs
+++ crates/task/src/import_feeds.rs
@@ -2,7 +2,6 @@ use std::sync::Arc;
 
 use apalis::prelude::{Data, Storage};
 use apalis_redis::RedisStorage;
-use redis::aio::MultiplexedConnection;
 use tokio::sync::Mutex;
 use url::Url;
 
@@ -21,7 +20,7 @@ impl Job {
 
 pub async fn run(
     job: Job,
-    data: Data<Arc<Mutex<RedisStorage<scrape_feed::Job, MultiplexedConnection>>>>,
+    data: Data<Arc<Mutex<RedisStorage<scrape_feed::Job>>>>,
 ) -> Result<(), apalis::prelude::Error> {
     tracing::debug!("Importing {} feeds", job.urls.len());
 
