f8177db22d5e79c40e286db77acf13843c53701c
Abhi Thube <athube@abhithube.com>
2025-02-04 20:10:07 -0800

    Removed config crate

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, str::FromStr, sync::Arc, time::Duration};
+use std::{borrow::Cow, error::Error, str::FromStr, sync::Arc, time::Duration};
 
 use apalis::{
     layers::WorkerBuilderExt,
@@ -35,8 +35,9 @@ use colette_task::{
     archive_thumbnail, import_bookmarks, import_feeds, refresh_feeds, scrape_bookmark, scrape_feed,
 };
 use redis::Client;
-use reqwest::ClientBuilder;
+use reqwest::{ClientBuilder, Url};
 use s3::{creds::Credentials, Bucket, BucketConfiguration, Region};
+use serde::{Deserialize, Deserializer};
 use sqlx::{Pool, Postgres};
 use tokio::{net::TcpListener, sync::Mutex};
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
@@ -46,6 +47,78 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 #[derive(Clone, rust_embed::Embed)]
 #[folder = "$CARGO_MANIFEST_DIR/../web/dist/"]
 struct Asset;
+
+#[derive(Clone, Debug, serde::Deserialize)]
+pub struct AppConfig {
+    #[serde(default = "default_host")]
+    pub host: String,
+    #[serde(default = "default_port")]
+    pub port: u16,
+    pub database_url: String,
+    pub redis_url: String,
+    pub aws_access_key_id: Cow<'static, str>,
+    pub aws_secret_access_key: Cow<'static, str>,
+    #[serde(default = "default_aws_region")]
+    pub aws_region: Cow<'static, str>,
+    #[serde(default = "default_bucket_name")]
+    pub bucket_name: Cow<'static, str>,
+    #[serde(default = "default_bucket_endpoint_url")]
+    pub bucket_endpoint_url: Url,
+    #[serde(deserialize_with = "string_to_vec", default = "default_origin_urls")]
+    pub origin_urls: Vec<String>,
+    #[serde(default = "default_refresh_enabled")]
+    pub refresh_enabled: bool,
+    #[serde(default = "default_cron_refresh")]
+    pub cron_refresh: String,
+    #[serde(default = "default_api_prefix")]
+    pub api_prefix: String,
+}
+
+fn default_host() -> String {
+    "0.0.0.0".to_owned()
+}
+
+fn default_port() -> u16 {
+    8000
+}
+
+fn default_aws_region() -> Cow<'static, str> {
+    Cow::Borrowed("us-east-1")
+}
+
+fn default_bucket_name() -> Cow<'static, str> {
+    Cow::Borrowed("colette")
+}
+
+fn default_bucket_endpoint_url() -> Url {
+    "http://localhost:9000".parse().unwrap()
+}
+
+pub fn string_to_vec<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value: String = Deserialize::deserialize(deserializer)?;
+    let parsed = value.split(',').map(|s| s.trim().to_owned()).collect();
+
+    Ok(parsed)
+}
+
+fn default_origin_urls() -> Vec<String> {
+    Vec::new()
+}
+
+fn default_refresh_enabled() -> bool {
+    true
+}
+
+fn default_cron_refresh() -> String {
+    "0 */15 * * * *".to_owned()
+}
+
+fn default_api_prefix() -> String {
+    "/api/v1".to_owned()
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -67,7 +140,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .init();
     }
 
-    let app_config = colette_config::load_config()?;
+    let app_config = envy::from_env::<AppConfig>()?;
 
     let pool = Pool::<Postgres>::connect(&app_config.database_url).await?;
     sqlx::migrate!("../../migrations").run(&pool).await?;
