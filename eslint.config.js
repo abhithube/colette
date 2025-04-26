8e34f948a6bf6136862af6f4895b72f13ce1adf4
Abhi Thube <athube@abhithube.com>
2025-04-26 03:03:38 -0700

    Updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,29 @@
-import { includeIgnoreFile } from '@eslint/compat'
-import pluginJs from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import pluginReact from 'eslint-plugin-react'
-import reactCompiler from 'eslint-plugin-react-compiler'
+import eslint from '@eslint/js'
+import prettier from 'eslint-config-prettier/flat'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import globals from 'globals'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import tseslint from 'typescript-eslint'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const gitignorePath = path.resolve(__dirname, '.gitignore')
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat['jsx-runtime'],
+export default tseslint.config(
+  {
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['**/dist/', '**/node_modules/'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
   {
     plugins: {
       'react-hooks': reactHooks,
     },
-    rules: reactHooks.configs.recommended.rules,
-  },
-  {
-    plugins: {
-      'react-compiler': reactCompiler,
-    },
     rules: {
-      'react-compiler/react-compiler': 'error',
+      'react-hooks/react-compiler': 'error',
     },
   },
-  eslintConfigPrettier,
-  includeIgnoreFile(gitignorePath),
-  {
-    ignores: ['**/dist/', 'apps/web/src/components/ui/'],
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
-    },
-  },
-]
+  prettier,
+)
